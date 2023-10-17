9df217c2e428105db630191e9231738035e2c5ba
urtanto <anikanovvlad@yandex.ru>
2023-10-18 00:23:49 +0300

    I have fixed CG and refactored code

--- anikanov.vladislav/P1/main.cpp
+++ anikanov.vladislav/P1/main.cpp
@@ -1,38 +1,25 @@
 #include <iostream>
 #include <limits>
 
-#define ll long long
-
-void inputNumber(ll &number)
-{
-    using namespace std;
-    char input_number[20];
-    try {
-        cin >> input_number;
-        number = stoll(input_number);
-    } catch (std::logic_error &error) {
-        cout << "Неправильный ввод\n";
-        exit(2);
-    }
-}
-
 int main()
 {
-    using namespace std;
-    ll max_count = numeric_limits<long long>::max(), count = 0, min_member = numeric_limits<long long>::max();
-    ll number;
-    inputNumber(number);
+  const long long max_count = std::numeric_limits<long long>::max();
+  long long min_member = max_count;
+  long long count, number;
+  try {
+    if (!(std::cin >> number)) throw std::logic_error("Wrong input");
     while (number != 0) {
-        if (number == min_member && count < max_count) ++count;
-        if (number == min_member && count == max_count) {
-            cout << "Слишком много минимальных элементов\n";
-            exit(2);
-        }
-        if (number < min_member) {
-            min_member = number;
-            count = 1;
-        }
-        inputNumber(number);
+      if (number == min_member && count < max_count) ++count;
+      if (number == min_member && count == max_count) throw std::logic_error("To much minimal numbers");
+      if (number < min_member) {
+        min_member = number;
+        count = 1;
+      }
+      if (!(std::cin >> number)) throw std::logic_error("Wrong input");
     }
-    cout << count << "\n";
+  } catch (std::logic_error const &error) {
+    std::cout << error.what() << "\n";
+    return 2;
+  }
+  std::cout << count << "\n";
 }
