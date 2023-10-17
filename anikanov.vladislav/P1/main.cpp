1db2a3a043ac0acf0c237851685520e1f3bf9571
urtanto <anikanovvlad@yandex.ru>
2023-10-18 00:34:03 +0300

    Exceptions were changed

--- anikanov.vladislav/P1/main.cpp
+++ anikanov.vladislav/P1/main.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 #include <limits>
 
-int main()
+void solving(long long &count)
 {
   const long long max_count = std::numeric_limits<long long>::max();
   long long min_member = max_count;
-  long long count, number;
-  try {
-    if (!(std::cin >> number)) throw std::logic_error("Wrong input");
-    while (number != 0) {
-      if (number == min_member && count < max_count) ++count;
-      if (number == min_member && count == max_count) throw std::logic_error("To much minimal numbers");
-      if (number < min_member) {
-        min_member = number;
-        count = 1;
-      }
-      if (!(std::cin >> number)) throw std::logic_error("Wrong input");
+  long long number;
+  if (!(std::cin >> number)) throw std::invalid_argument("Wrong input");
+  while (number != 0) {
+    if (number == min_member && count < max_count) ++count;
+    if (number == min_member && count == max_count) throw std::overflow_error("To much minimal numbers");
+    if (number < min_member) {
+      min_member = number;
+      count = 1;
     }
+    if (!(std::cin >> number)) throw std::invalid_argument("Wrong input");
+  }
+}
+
+int main()
+{
+  long long count;
+  try {
+    solving(count);
   } catch (std::logic_error const &error) {
     std::cout << error.what() << "\n";
     return 2;
