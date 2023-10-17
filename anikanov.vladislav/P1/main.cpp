6221f9192a0fa818e21a6394ba5afc30355c71da
urtanto <anikanovvlad@yandex.ru>
2023-10-17 21:08:48 +0300

    some extra fixes

--- anikanov.vladislav/P1/main.cpp
+++ anikanov.vladislav/P1/main.cpp
@@ -3,27 +3,29 @@
 
 #define ll long long
 
-inline void inputNumber(ll &number)
+void inputNumber(ll &number)
 {
+    using namespace std;
     char input_number[20];
     try {
-        std::cin >> input_number;
-        number = std::stoll(input_number);
+        cin >> input_number;
+        number = stoll(input_number);
     } catch (std::logic_error &error) {
-        std::cout << "Неправильный ввод\n";
+        cout << "Неправильный ввод\n";
         exit(2);
     }
 }
 
 int main()
 {
-    ll max_count = std::numeric_limits<long long>::max(), count = 0, min_member = std::numeric_limits<long long>::max();
+    using namespace std;
+    ll max_count = numeric_limits<long long>::max(), count = 0, min_member = numeric_limits<long long>::max();
     ll number;
     inputNumber(number);
     while (number != 0) {
         if (number == min_member && count < max_count) ++count;
         if (number == min_member && count == max_count) {
-            std::cout << "Слишком много минимальных элементов\n";
+            cout << "Слишком много минимальных элементов\n";
             exit(2);
         }
         if (number < min_member) {
@@ -32,5 +34,5 @@ int main()
         }
         inputNumber(number);
     }
-    std::cout << count << "\n";
+    cout << count << "\n";
 }
