b00f888269699c9ee71a8ed9cf5e41cf35157412
0412erik <litsinger00@bk.ru>
2023-10-21 19:47:39 +0300

    P1

--- litsinger.ernest/P1/main.cpp
+++ litsinger.ernest/P1/main.cpp
@@ -11,45 +11,45 @@ int main()
   std::cin << n;
   if (n == 0)
   {
-	std::cout << "Short sequence\n";
-	return 2;
+    std::cout << "Short sequence\n";
+    return 2;
   }
   if (std::cin.fail())
   {
-	std::cout << "The elements must be specified by numbers\n";
-	return 1;
+    std::cout << "The elements must be specified by numbers\n";
+    return 1;
   }
   if (n != static_cast<int>(n))
   {
     std::cout("Only int");
-	return 1;
+    return 1;
   }
   if (n > maxlim || n < minlim)
   {
-	std::cout << "Overflow";
-	return 1;
+    std::cout << "Overflow";
+    return 1;
   }
   int mx;
   mx = n;
   while (n != 0)
   {
-	std::cin << n;
-	if (std::cin.fail())
-	{
-	  std::cout << "The elements must be specified by numbers\n";
-	  return 1;
-	}
-	if (n != static_cast<int>(n))
-	{
-	  std::cout("Only int");
-	  return 1;
-	}
-	if (n > maxlim || n < minlim)
-	{
-	  std::cout << "Overflow";
-	  return 1;
-	}
-	kk = maxelement(int n, int k, int mx)
+    std::cin << n;
+    if (std::cin.fail())
+    {
+      std::cout << "The elements must be specified by numbers\n";
+      return 1;
+    }
+      if (n != static_cast<int>(n))
+    {
+      std::cout("Only int");
+      return 1;
+    }
+    if (n > maxlim || n < minlim)
+    {
+      std::cout << "Overflow";
+      return 1;
+    }
+    kk = maxelement(int n, int k, int mx)
   }
   std::cout << kk;
   return 0;
