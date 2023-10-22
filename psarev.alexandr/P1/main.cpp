ea970cf9fc64ad66dd14d0899d38253a12e708fc
AzZR43L <alexandrpsarev5555@gmail.com>
2023-10-23 00:03:19 +0500

    codestyle for tests fixes

--- psarev.alexandr/P1/main.cpp
+++ psarev.alexandr/P1/main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
-
-int main() 
+int main()
 {
 	int value;
-	
 	if (!(std::cin >> value)) {
 		std::cout << "Incorrect input!";
 		return 1;
 	}
-
 	int prev_value = value;
 	int seq_len = 0;
 	int max_len = 0;
-
 	while (value != 0) {
-
 		if (value >= prev_value) {
 			seq_len++;
 			if (seq_len > max_len) {
@@ -23,14 +18,12 @@ int main()
 		} else {
 			seq_len = 1;
 		}
-
 		prev_value = value;
 		if (!(std::cin >> value)) {
 			std::cout << "Incorrect input!";
 			return 1;
 		}
 	}
-
 	std::cout << max_len << std::endl;
 	return 0;
 }
