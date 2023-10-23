676b7968f7dda43f456fe30db4edbd27c2976e87
AlexNik <sahan@DESKTOP-R8OTJOJ>
2023-10-24 01:44:15 +0300

    fixes

--- nikiforov.alexander/P1/main.cpp
+++ nikiforov.alexander/P1/main.cpp
@@ -3,15 +3,15 @@
 
 int main()
 {
-	int countmin = 0;
-	try
-	{
-		countmin = func(std::cin);
-		std::cout << countmin;
-	}
-	catch (const std::exception& e)
-	{
-		std::cout << e.what();
-	  return 1;
-	}
+  int countmin = 0;
+  try
+  {
+    countmin = func(std::cin);
+    std::cout << countmin;
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what();
+    return 1;
+  }
 }
--- nikiforov.alexander/P1/source.cpp
+++ nikiforov.alexander/P1/source.cpp
@@ -4,28 +4,26 @@
 int func(std::istream&)
 {
   int num;
-
-	if (!(std::cin >> num)) {
-		throw std::invalid_argument("Wrong input");
-	}
-	int beforenum = num; 
+  if (!(std::cin >> num)) {
+    throw std::invalid_argument("Wrong input");
+  }
+  int beforenum = num; 
   int count = 0; 
   int flag = 0; 
   int length = 0;
-	while (num != 0)
-	{
-		if (num < beforenum) {
-			flag = 1;
-		}
-		else if (num > beforenum && flag == 1) {
-			flag = 0;
-			count++;
-		}
-		length++;
-		beforenum = num;
-		if (!(std::cin >> num)) {
-			throw std::invalid_argument("Wrong input");
-		}
-	}
-	return count;
+  while (num != 0)
+  {
+    if (num < beforenum) {
+      flag = 1;
+    } else if (num > beforenum && flag == 1) {
+      flag = 0;
+      count++;
+    }
+    length++;
+    beforenum = num;
+    if (!(std::cin >> num)) {
+      throw std::invalid_argument("Wrong input");
+    }
+  }
+  return count;
 }
