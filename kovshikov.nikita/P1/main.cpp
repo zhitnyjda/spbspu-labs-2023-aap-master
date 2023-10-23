5feb1469a84f03681ae30ebf499dd2c2727e95d4
harduserNIKITA <cumpotttt@yandex.ru>
2023-10-23 23:51:30 +0300

    please...

--- kovshikov.nikita/P1/main.cpp
+++ kovshikov.nikita/P1/main.cpp
@@ -2,31 +2,31 @@
 #include "count_quantity_even_num.hpp"
 int main()
 {
-	int num = 0;
-	int quantity = 0;
-	int max_quantity = 0;
-	std::cin >> num;
-	while (true)
-	{
-		if (!std::cin)    //проверка что на ввод подается целое число и его размеры умещаются в диапазон int
-		{
-			std::cout << "error";
-			return 1;     //в случае неверного введения программа возвращает 1
-		}
-		else if (num == 0)
-		{
-			std::cout << max_quantity << "\n";
-			return 0;
-		}
-		else
-		{
-			quantity = count_quantity_even_num(num, quantity);
-			if (quantity >= max_quantity)  // если текущаю последовательность больше максимальной 
-			{
-				max_quantity = quantity;  // то максимальная принимает значение текущей
-			}
-			std::cin >> num;
-		}
-	}
-	return 0;
+  int num = 0;
+  int quantity = 0;
+  int max_quantity = 0;
+  std::cin >> num;
+  while (true)
+  {
+    if (!std::cin)
+    {
+      std::cout << "error\n";
+      return 1;
+    }
+    else if (num == 0)
+    {
+      std::cout << max_quantity << "\n";
+      return 0;
+    }
+    else
+    {
+      quantity = count_quantity_even_num(num, quantity);
+      if (quantity >= max_quantity)
+      {
+        max_quantity = quantity;
+      }
+      std::cin >> num;
+    }
+  }
+  return 0;
 }
