fb81b0bd4bb4c2ebd14142d075233cb4c717b5f2
han7ter <sobmisha12@gmail.com>
2023-10-23 19:38:10 +0300

    corrected the defects

--- sobolevsky.misha/P1/main.cpp
+++ sobolevsky.misha/P1/main.cpp
@@ -23,7 +23,7 @@ int main()
   }
   catch (const std::logic_error & e)
   {
-    std::cerr << e.what() << '\n';
+    std::cerr << e.what() << "\n";
     return 1;
   }
   std::cout << countMax << "\n";
