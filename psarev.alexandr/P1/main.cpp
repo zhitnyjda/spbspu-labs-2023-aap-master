5b6e301bffb4c4248130ab9b7eb8fc5c8ae6e5b9
AzZR43L <alexandrpsarev5555@gmail.com>
2023-10-23 22:42:28 +0500

    indentation fix (mb fin)

--- psarev.alexandr/P1/main.cpp
+++ psarev.alexandr/P1/main.cpp
@@ -5,31 +5,31 @@ using std::endl;
 
 int main()
 {
-	int value;
-	if (!(cin >> value)) {
-		cout << "Incorrect input!\n";
-		return 1;
-	}
+  int value;
+  if (!(cin >> value)) {
+    cout << "Incorrect input!\n";
+    return 1;
+  }
 
-	int prev_value = value;
-	int seq_len = 0;
-	int max_len = 0;
+  int prev_value = value;
+  int seq_len = 0;
+  int max_len = 0;
 
-	while (value != 0) {
-		if (value >= prev_value) {
-			seq_len++;
-			if (seq_len > max_len) {
-				max_len = seq_len;
-			}
-		} else {
-			seq_len = 1;
-		}
-		prev_value = value;
-		if (!(cin >> value)) {
-			cout << "Incorrect input!\n";
-			return 1;
-		}
-	}
-	cout << max_len << endl;
-	return 0;
+  while (value != 0) {
+    if (value >= prev_value) {
+      seq_len++;
+      if (seq_len > max_len) {
+        max_len = seq_len;
+      }
+    } else {
+      seq_len = 1;
+    }
+    prev_value = value;
+    if (!(cin >> value)) {
+      cout << "Incorrect input!\n";
+      return 1;
+    }
+  }
+  cout << max_len << endl;
+  return 0;
 }
