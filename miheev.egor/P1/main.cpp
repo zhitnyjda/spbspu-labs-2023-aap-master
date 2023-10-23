6fb66f4fa84aa0fa17f47b4cdc937c458497ce9e
Miheev Egor <etdiary1@gmail.com>
2023-10-23 16:51:02 +0300

    First functional solution. Not perfect, but still

--- miheev.egor/P1/main.cpp
+++ miheev.egor/P1/main.cpp
@@ -1,33 +1,56 @@
 #include <iostream>
 
+void put_number_from_cin_to(int & variable)
+{
+  std::cin >> variable;
+  if (!std::cin)
+  {
+    throw std::invalid_argument("Your input can't be interpret as a sequence of numbers\n");
+  }
+}
+
 int main()
 {
-  int current_number = 0;
-  int previous_number = 0;
-  int count = 0;
-
-  std::cin >> current_number;
-
-  while(std::cin)
+  try
   {
-    previous_number = current_number;
-    if (previous_number == 0){
-      std::cout << "Programm can't work with such sequence. Maybe it's too short?";
-      return 2;
-    }
+    int current_number = 0;
+    int previous_number = 0;
+    int count = -1; 
 
-    std::cin >> current_number;
+    put_number_from_cin_to(current_number);
 
-    if (current_number == 0)
+    while(current_number != 0)
     {
-      std::cout << count << '\n';
-      return 0;
+      previous_number = current_number;
+
+      put_number_from_cin_to(current_number);
+
+      if (current_number != 0)
+      {
+        count = (count == -1) ? 0 : count;
+        if (current_number%previous_number == 0)
+        {
+          count++;
+        }
+      }
     }
-    else if (current_number%previous_number == 0)
+    if (count == -1)
     {
-      count++;
+      throw std::logic_error("Can't work with such sequence. Maybe it's too short?\n");
     }
+    std::cout << count << '\n';
+    return 0;
   }
-  std::cout << "Something you considered number sequence can't be interpret this way.\n Please, use numbers next time\n";
-  return 1;
+
+  catch (const std::invalid_argument& error)
+  {
+    std::cout << error.what();
+    return 1;
+  }
+
+  catch (const std::logic_error& error)
+  {
+    std::cout << error.what();
+    return 2;
+  }
 }
