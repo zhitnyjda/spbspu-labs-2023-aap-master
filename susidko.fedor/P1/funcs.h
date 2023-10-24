b0db446e377d6d7ca27ce9ac55e9ebc392b0fc7c
F-psss <fpvadimovich@gmail.com>
2023-10-24 12:58:01 +0300

    FINAL FINAL

--- susidko.fedor/P1/funcs.h
+++ susidko.fedor/P1/funcs.h
@@ -1,7 +1,10 @@
 #ifndef FUNCS_H
 #define FUNCS_H
-int getSign(int value);
-int sum(int a, int b);
-int read(int second_elem);
-bool isSameSign(int a, int b);
+namespace susidko
+{
+  int getSign(int value);
+  int sum(int a, int b);
+  int read(int second_elem);
+  bool isSameSign(int a, int b);
+}
 #endif
--- susidko.fedor/P1/main.cpp
+++ susidko.fedor/P1/main.cpp
@@ -3,11 +3,6 @@
 #include <limits>
 #include "funcs.h"
 
-int read(int second_elem);
-int getSign(int value);
-bool isSameSign(int a, int b);
-int sum(int a, int b);
-
 int main()
 {
   int first_elem = 0;
@@ -17,17 +12,17 @@ int main()
   int cou = 0;
   try
   {
-    forth_elem = read(first_elem);
+    forth_elem = susidko::read(first_elem);
     while (forth_elem != 0)
     {
-      if ((forth_elem == sum(second_elem, third_elem)) && (second_elem != 0))
+      if ((forth_elem == susidko::sum(second_elem, third_elem)) && (second_elem != 0))
       {
         cou += 1;
       }
       first_elem = second_elem;
       second_elem = third_elem;
       third_elem = forth_elem;
-      forth_elem = read(first_elem);
+      forth_elem = susidko::read(first_elem);
     }
     std::cout << cou << "\n";
     return 0;
