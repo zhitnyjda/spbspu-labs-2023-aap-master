ff8e8c222cd5a2960328d45ed9343006195f8f96
0412erik <litsinger00@bk.ru>
2023-10-25 18:11:11 +0300

    p1 fiiix

---
+++ litsinger.ernest/P1/main.cpp
@@ -0,0 +1,33 @@
+#include "maxcount.hpp"
+#include <iostream>
+#include <exception>
+
+int main()
+{
+  int main = 0;
+  try
+  {
+    int counter = 0;
+    int length = -1;
+    int value = 1;
+    std::cin.exceptions(std::istream::failbit);
+    while (value != 0)
+    {
+      std::cin >> value;
+      maxCount(counter, max, value);
+      length++;
+    }
+    if (length == 0)
+    {
+      std::cout << "Short sequence" << "\n";
+      return 2;
+    }
+    std::cout << counter << "\n";
+    return 0;
+  }
+  catch (const std::istream::failure & e)
+  {
+    std::cer << "Input error" << "\n";
+    return 1;
+  }
+}
