89b6c7e6fd3056c63a045e88dbfdd547137e5617
jirkov17 <JIRKOFF17@GMAIL.COM>
2023-10-26 20:27:13 +0300

    P1 fixed main.cpp filr

--- jirkov.vladimir/P1/main.cpp
+++ jirkov.vladimir/P1/main.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
-int main() {
+int main()
+{
     int count = 0;
     int previous, current, next;
     std::cin >> previous;
-    if (previous == 0) {
+    if (previous == 0)
+    {
         std::cout << "Последовательность слишком короткая " << count << std::endl;
         return 2;
     }
     std::cin >> current;
-    if (current == 0) {
+    if (current == 0)
+    {
         std::cout << "Последовательность слишком короткая " << count << std::endl;
         return 2;
     }
-    while (true) {
+    while (true)
+    {
         std::cin >> next;
-        if (next == 0) {
-            break; 
+        if (next == 0)
+        {
+            break;
         }
-        if (current < previous && current > next) {
+        if (current < previous && current > next)
+        {
             count++;
         }
         previous = current;
         current = next;
     }
-
-    std::cout << "Количество элементов, которые меньше предыдущего элемента, но больше следующего: " << count << std::endl;
-
+    std::cout << "Количество элементов: " << count << std::endl;
     return 1;
 }
+
