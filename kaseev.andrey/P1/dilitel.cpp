152ad9e9f8ddc46673cf5f32d27e2db93857e5f6
herznaetkcto <147743642+herznaetkcto@users.noreply.github.com>
2023-10-26 23:56:38 +0300

    P1 first try

---
+++ kaseev.andrey/P1/dilitel.cpp
@@ -0,0 +1,34 @@
+#include "dilitel.h"
+#include <iostream>
+#include <stdexcept>
+#include <limits>
+
+int dilitel(std::istream &cin)
+{
+  const int max_count = std::numeric_limits<int>::max();
+  int count = 0;
+  int firstnum;
+  int secondnum;
+  if (!(std::cin >> firstnum) || firstnum == 0) {
+    throw std::logic_error("Wrong input");
+  }
+  if (!(std::cin >> secondnum)) {
+    throw std::logic_error("Wrong input");
+  }
+  if (firstnum == 0 || secondnum == 0) {
+    throw std::logic_error("Not enough elements");
+  }
+  while (secondnum != 0 && firstnum != 0) {
+    if (secondnum % firstnum == 0) {
+      count += 1;
+    }
+    firstnum = secondnum;
+    if (count > max_count) {
+      throw std::overflow_error("Too many elements");
+    }
+    if (!(cin >> secondnum)) {
+      throw std::logic_error("Wrong input");
+    }
+  }
+  return count;
+}
---
+++ kaseev.andrey/P1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "dilitel.cpp"
+#include <stdexcept>
+
+int main()
+{
+  try {
+    int ans = 0;
+    ans = dilitel(std::cin);
+    std::cout << ans << "\n";
+    return 0;
+  }
+  catch (const std::logic_error &error) {
+    std::cerr << error.what() << "\n";
+    return 2;
+  }
+}
