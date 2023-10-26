bda7c7656d081e19827d577fbb62272a6efb9985
jirkov17 <JIRKOFF17@GMAIL.COM>
2023-10-26 21:35:11 +0300

    P1 fixed main.cpp file1

--- jirkov.vladimir/P1/main.cpp
+++ jirkov.vladimir/P1/main.cpp
@@ -2,34 +2,33 @@
 int main()
 {
     int count = 0;
-    int previous, current, next;
+    unsigned int previous, current, next;
     std::cin >> previous;
     if (previous == 0)
     {
-        std::cout << "Последовательность слишком короткая " << count << std::endl;
-        return 2;
+      std::cout << "Последовательность короткая " << count << "\n";
+      return 2;
     }
     std::cin >> current;
     if (current == 0)
     {
-        std::cout << "Последовательность слишком короткая " << count << std::endl;
-        return 2;
+      std::cout << "Последовательность короткая " << count << "\n";
+      return 2;
     }
     while (true)
     {
-        std::cin >> next;
-        if (next == 0)
-        {
-            break;
-        }
-        if (current < previous && current > next)
-        {
-            count++;
-        }
-        previous = current;
-        current = next;
+      std::cin >> next;
+      if (next == 0)
+      {
+        break;
+      }
+      if (current < previous && current > next)
+      {
+        count++;
+      }
+      previous = current;
+      current = next;
     }
-    std::cout << "Количество элементов: " << count << std::endl;
+    std::cout << "Количество элементов: " << count << "\n";
     return 1;
 }
-
