3f55661ad2e70460779f3abc6a761e1195ac2bf5
jirkov17 <JIRKOFF17@GMAIL.COM>
2023-10-27 18:23:06 +0300

    1

--- jirkov.vladimir/P1/main.cpp
+++ jirkov.vladimir/P1/main.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
-int main()
+int main() 
 {
   int count = 0;
   int previous, current, next;
   std::cin >> previous;
-  if (previous == 0)
+  if (previous == 0) 
   {
     std::cout << "Последовательность короткая  " << count << "\n";
     return 2;
   }
   std::cin >> current;
-  if (current == 0)
+  if (current == 0) 
   {
     std::cout << "Последовательность короткая  " << count << "\n";
     return 2;
   }
-  while (true)
+  while (true) 
   {
     std::cin >> next;
-    if (next == 0)
+    if (next == 0) 
     {
-      break;
+      break; 
     }
-    if (current < previous && current > next)
+    if (current < previous && current > next) 
     {
       count++;
     }
     previous = current;
     current = next;
-  }
+    }
   std::cout << "Количество элементов: " << count << "\n";
   return 1;
 }
