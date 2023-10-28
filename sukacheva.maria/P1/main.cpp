091262a191476d4c44f70985ab1ba87797318101
oksidmargantsa <sukachva.maria@mail.ru>
2023-10-28 15:00:35 +0300

    P1 lab is ready

---
+++ sukacheva.maria/P1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <limits>
+#include "main.hpp"
+
+int main()
+{
+  SomeFunctor COUNT;
+  long long value = 0;
+  do
+  {
+    std::cin >> value;
+    if (!std::cin)
+    {
+      std::cerr << "Wrong input.\n";
+      return 1;
+    }
+    else if (value != 0)
+    {
+      try
+        {
+          COUNT(value);;
+	}
+      catch (const std::exception& e)
+	{
+          std::cerr << "Error: " << e.what() << "\n";
+          return 1;
+	}
+    }
+  }
+  while (value != 0);
+  std::cout << COUNT.functor() << "\n";
+  return 0;
+}
