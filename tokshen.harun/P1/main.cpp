dc8c7c7efaae2f579350aadcd4c74ee58d8b7ec4
Harunerd <harun.toksen45@gmail.com>
2023-10-29 23:52:05 +0300

    Fix \n

--- tokshen.harun/P1/main.cpp
+++ tokshen.harun/P1/main.cpp
@@ -7,9 +7,9 @@ int main()
   try {
     answer = findSecondMax(std::cin);
   } catch (const std::exception & err) {
-    std::cout << err.what() << std::endl;
+    std::cout << err.what() << "\n";
     return 2;
   }
-  std::cout << answer << std::endl;
+  std::cout << answer << "\n";
   return 0;
 }
