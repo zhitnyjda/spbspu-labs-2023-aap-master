7db00817eae683d7ff459a38266e2a70449fb35c
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-10-31 03:43:19 +0300

    Create main.cpp

---
+++ zhitnyj.dmitriy/P1/EQL-SEQ/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "helpersFunc.h"
+
+int main()
+{
+	if(!std::cin)
+	{
+		std::cerr << "Can't identify given data as sequence!" << std::endl;
+
+		return 1;
+	}
+	helperFunc * helperFunc_ = new helperFunc(&std::cin);
+
+	helperFunc_->findCount();
+
+	return 0;
+}
