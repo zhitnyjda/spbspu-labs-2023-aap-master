ec529d434c5385f8406492d73e7cca56e6e68b3e
yva <yartsev-va@yandex.ru>
2023-11-04 21:13:05 +0300

    Reworked input; Processed wrong input

--- yartsev.viktor/P1/main.cpp
+++ yartsev.viktor/P1/main.cpp
@@ -2,10 +2,17 @@
 
 int main ()
 {
-  int x;
-  while (std::cin) {
-    std::cin >> x;
-  }
-  std::cout << x << "\n";
+  int num;
+  int counter = 0;
+  do {
+    std::cin >> num;
+    if (!std::cin) {
+      std::cerr << "Not a sequence\n";
+      return 1;
+    } else if (num != 0) {
+      counter++;
+    }
+  } while (num != 0);
+  std::cout << counter << "\n";
   return 0;
 }
