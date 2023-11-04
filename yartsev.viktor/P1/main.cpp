1ecfa7085cc4803fe428ebd085134efe304bbb1e
yva <yartsev-va@yandex.ru>
2023-11-04 22:43:33 +0300

    Added overflow error exception

--- yartsev.viktor/P1/main.cpp
+++ yartsev.viktor/P1/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <limits>
+#include <stdexcept>
 
 struct Counter {
   public: 
     void operator()(const int num)
     {
-      num % 2 == 0 ? currentSequence++ : currentSequence = 0;
+      const int maxSize = std::numeric_limits< int >::max();
+      if (num % 2 == 0) {
+        if (currentSequence == maxSize) {
+          throw std::overflow_error("Sequence is too long");
+        } else {
+          currentSequence++;
+        }
+      } else {
+        currentSequence = 0;
+      }
       maxSequence = std::max(maxSequence, currentSequence);
     }
     int getMaxSequence()
@@ -23,10 +34,16 @@ int main ()
   do {
     std::cin >> num;
     if (!std::cin) {
-      std::cerr << "Not a sequence\n";
+      std::cerr << "Error: Not a sequence\n";
       return 1;
     } else if (num != 0) {
-      counter(num);
+      try {
+        counter(num);
+      } catch (const std::exception & e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        std::cout << counter.getMaxSequence() << "\n";
+        return 2;
+      }
     }
   } while (num != 0);
   std::cout << counter.getMaxSequence() << "\n";
