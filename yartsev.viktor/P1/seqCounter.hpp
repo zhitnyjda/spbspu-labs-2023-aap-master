81328946491505dee427210724bfeb04e436d713
yva <yartsev-va@yandex.ru>
2023-11-04 23:08:04 +0300

    Added header

---
+++ yartsev.viktor/P1/seqCounter.hpp
@@ -0,0 +1,33 @@
+#ifndef SEQCOUNTER
+#define SEQCOUNTER
+
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+
+struct Counter {
+  public: 
+    void operator()(const int num)
+    {
+      const int maxSize = std::numeric_limits< int >::max();
+      if (num % 2 == 0) {
+        if (currentSequence == maxSize) {
+          throw std::overflow_error("Sequence is too long");
+        } else {
+          currentSequence++;
+        }
+      } else {
+        currentSequence = 0;
+      }
+      maxSequence = std::max(maxSequence, currentSequence);
+    }
+    int getMaxSequence()
+    {
+      return maxSequence;
+    }
+  private:
+    int maxSequence = 0;
+    int currentSequence = 0;
+};
+
+#endif
