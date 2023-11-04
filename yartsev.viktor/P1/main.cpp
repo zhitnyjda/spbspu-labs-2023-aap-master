ae5100337b00e864ca83bbfd973061daacaf8c03
yva <yartsev-va@yandex.ru>
2023-11-04 21:53:56 +0300

    Implemented struct Counter

--- yartsev.viktor/P1/main.cpp
+++ yartsev.viktor/P1/main.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 
+struct Counter {
+  int amount = 0; 
+  void addNumber(const int num)
+  {
+    amount += (num % 2 == 0);
+  }
+  int getAmount()
+  {
+    return amount;
+  }
+};
+
 int main ()
 {
   int num;
-  int counter = 0;
+  Counter counter; 
   do {
     std::cin >> num;
     if (!std::cin) {
       std::cerr << "Not a sequence\n";
       return 1;
     } else if (num != 0) {
-      counter++;
+      counter.addNumber(num);
     }
   } while (num != 0);
-  std::cout << counter << "\n";
+  std::cout << counter.getAmount() << "\n";
   return 0;
 }
