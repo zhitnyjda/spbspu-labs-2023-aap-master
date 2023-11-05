3dcd377f498e8f997a0c61c5a455b9050bb8b539
0412erik <litsinger00@bk.ru>
2023-11-05 22:14:35 +0300

    fix

--- litsinger.ernest/P1/maxchetn.cpp
+++ litsinger.ernest/P1/maxchetn.cpp
@@ -1,10 +1,7 @@
 #include "maxchetn.hpp"
-#include <limits>
-#include <stdexcept>
 
 void litsinger::count_max::operator()(int value)
 {
-  const int maxSize = std::numeric_limits< int >::max();
   if (max == 0)
   {
     max = value;
