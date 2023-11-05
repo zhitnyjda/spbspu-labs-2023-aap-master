ebd0bbb698402df0c35440eff64b06aedf3bed18
ilyapanov123 <89517673116g@gmail.com>
2023-11-05 16:07:02 +0300

    [SGN-CHG] Определить количество перемен знаков последовательности

--- panov.ilya/P1/main.cpp
+++ panov.ilya/P1/main.cpp
@@ -17,14 +17,14 @@ int main()
   }
   catch (const std::istream::failure& e)
   {
-    std::cerr << "Only number pls\n";
+    std::cerr << "Only number pls" << "\n";
     return 1;
   }
   catch (const std::logic_error& e)
   {
-    std::cerr << e.what() << '\n';
+    std::cerr << e.what() << "\n";
     return 1;
   }
-  std::cout << counterRr() << '\n';
+  std::cout << counterRr() << "\n";
   return 0;
 }
