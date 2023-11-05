cc3abd09cf5466cc715d7a05cb4c56021a87830e
ilyapanov123 <89517673116g@gmail.com>
2023-11-05 14:33:32 +0300

    [SGN-CHG] Определить количество перемен знаков последовательности

--- panov.ilya/P1/counterRr.cpp
+++ panov.ilya/P1/counterRr.cpp
@@ -1,9 +1,8 @@
 #include "counterRr.hpp"
-
 Panov::CounterRr::CounterRr():
   count(0), first(0)
-{}
-
+{
+}
 void Panov::CounterRr::operator()(int subsequent)
 {
   if ((first < 0 && subsequent > 0) || (first > 0 && subsequent < 0))
@@ -12,7 +11,6 @@ void Panov::CounterRr::operator()(int subsequent)
   }
   first = subsequent;
 }
-
 int Panov::CounterRr::operator()() const
 {
   return count;
