d48bf6538439a18523a0e3abfc83f618bd422561
ilyapanov123 <89517673116g@gmail.com>
2023-11-06 14:12:30 +0300

    P1

--- panov.ilya/P1/main.cpp
+++ panov.ilya/P1/main.cpp
@@ -4,28 +4,27 @@
 
 int main()
 {
-  panov::NumOfchar numOfchar;
-  try
-  {
-    std::cin.exceptions(std::istream::failbit);
-    int value = 0;
-    do
+    panov::NumOfchar numOfchar;
+    try
     {
-      std::cin >> value;
-      numOfchar(value);
+        std::cin.exceptions(std::istream::failbit);
+        int value = 0;
+        do
+        {
+            std::cin >> value;
+            numOfchar(value);
+        } while (value != 0);
     }
-    while (value != 0);
-  }
-  catch (const std::istream::failure & e)
-  {
-    std::cerr << "OnlyNumberPLS" << "\n";
-    return 1;
-  }
-  catch (const std::logic_error& e)
-  {
-    std::cerr << e.what() << "\n";
-    return 1;
-  }
-  std::cout << numOfchar() << "\n";
-  return 0;
+    catch (const std::istream::failure& e)
+    {
+        std::cerr << "OnlyNumberPLS" << "\n";
+        return 1;
+    }
+    catch (const std::logic_error& e)
+    {
+        std::cerr << e.what() << "\n";
+        return 1;
+    }
+    std::cout << numOfchar() << "\n";
+    return 0;
 }
--- panov.ilya/P1/numOfchar.cpp
+++ panov.ilya/P1/numOfchar.cpp
@@ -1,19 +1,19 @@
-#include "numOfchar.hpp"
+﻿#include "numOfchar.hpp"
 
-panov::NumOfchar::NumOfchar():
-  count(0), first(0)
+panov::NumOfchar::NumOfchar() :
+    count(0), first(0)
 {}
 
 void panov::NumOfchar::operator()(int subsequent)
 {
-  if ((first < 0 && subsequent > 0) || (first > 0 && subsequent < 0))
-  {
-    count += 1;
-  }
-  first = subsequent;
+    if ((first < 0 && subsequent > 0) || (first > 0 && subsequent < 0))
+    {
+        count += 1;
+    }
+    first = subsequent;
 }
 
 int panov::NumOfchar::operator()() const
 {
-  return count;
+    return count;
 }
