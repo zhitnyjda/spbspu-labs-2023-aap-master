4afeab095205157e07370a43043aca2c1f817ede
shavel0k <anna.reznikova@icloud.com>
2023-11-08 20:24:53 +0300

    P1 lab is ready (2 attempt)

---
+++ reznikova.anna/P1/main.cpp
@@ -0,0 +1,38 @@
+#include "FindEqSeq.hpp"
+#include "FindDecSeq.hpp"
+#include <iostream>
+#include <stdexcept>
+
+using namespace reznikova;
+
+int main()
+{
+  MaxEqualSequense maxEqualSequense;
+  MaxDecreasingSequence maxDecreasingSequence;
+  long long number = 0;
+  do
+  {
+   std::cin >> number;
+   if(!std::cin)
+   {
+     std::cerr << "Not a sequence.\n";
+     return 1;
+   }
+   else if (number != 0)
+   {
+     try
+     {
+       maxEqualSequense(number);
+       maxDecreasingSequence(number);
+     }
+     catch (const std::exception & e)
+     {
+       std::cerr << "Error: " << e.what() << "\n";
+       return 2;
+     }
+   }
+  }
+  while (number != 0);
+  std::cout << "максимальное число подряд идущих равных элементов: " << maxEqualSequense() << "\n";
+  std::cout << "наибольшая длина монотонно-убывающего фрагмента последовательности: " << maxDecreasingSequence() << "\n";
+}
