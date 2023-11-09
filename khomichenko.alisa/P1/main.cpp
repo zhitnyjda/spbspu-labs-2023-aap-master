a52afdddc215335c829f26fb29cf293dab504920
kh-alisa <alisakhomicheko@gmail.com>
2023-11-09 23:37:06 +0300

    now we can read seq

--- khomichenko.alisa/P1/main.cpp
+++ khomichenko.alisa/P1/main.cpp
@@ -2,10 +2,16 @@
 int main()
 {
   int number =0;
-  std::cin>> number;
-  if (!std::cin)
-  {
-    std::cerr<< "not a sequence\n";
-    return 1;
-  }
+  size_t seqLenght = 0;
+  do{
+    std::cin>> number;
+    if (!std::cin)
+    {
+      std::cerr<< "not a sequence\n";
+      return 1;
+    } else if (number != 0) {
+      ++seqLenght;
+    }
+  } while (number!=0);
+  std::cout<< seqLenght<< "\n";
 }
