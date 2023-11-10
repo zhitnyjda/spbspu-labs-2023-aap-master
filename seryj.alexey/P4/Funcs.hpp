1ebe3ca62aca534e28023fa7cc2e80d251b13460
SeryjAlexey <slekseyvs.2005@gmail.com>
2023-11-11 01:52:39 +0300

    у меня много коммитов я настраиваю visual под отступы

--- seryj.alexey/P4/Funcs.hpp
+++ seryj.alexey/P4/Funcs.hpp
@@ -2,18 +2,19 @@
 #define FUNCS_HPP
 namespace seryj
 {
-	int fillArguments(int, const char* []);
-	int read();
-	class Matrix
-	{
-	public:
-		int line;
-		int column;
-		int* values;
-		void fillArray();
-		void printAvgOfNeigbours()const;
-	private:
-		double findAverageOfNeighbours(int,int)const;
-	};
+  int fillArguments(int, const char* []);
+  int read();
+  class Matrix
+  {
+  public:
+  int line;
+  int column;
+  int* values;
+  void fillArray();
+  void printAvgOfNeigbours()const;
+  private:
+  double findAverageOfNeighbours(int,int)const;
+  };
 }
 #endif
+
