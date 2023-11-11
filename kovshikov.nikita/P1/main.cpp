2b6bbd8034b7fd893170c37875b4f39ce95535a9
harduserNIKITA <cumpotttt@yandex.ru>
2023-11-12 00:47:20 +0300

    laba1 is ready?

--- kovshikov.nikita/P1/main.cpp
+++ kovshikov.nikita/P1/main.cpp
@@ -1,42 +1,37 @@
 #include <iostream>
-#include "count_count.hpp"
-#include <limits>
-using namespace::kovshikov;
+#include "even_count.hpp"
+
 int main()
 {
   size_t num = 0;
   size_t max = 0;
+  using namespace::kovshikov;
+  EvenCount sequenceEven;
   try
   {
-    Counter sequenceCounter;
     do
     {
       std::cin >> num;
       if (!std::cin)
       {
-        throw std::invalid_argument("Incorrect input\n");
+        throw std::logic_error("Incorrect input\n");
       }
       else
       {
         if (num != 0)
         {
-          sequenceCounter(num);
-          max = sequenceCounter();
+          sequenceEven(num);
+          max = sequenceEven();
         }
       }
     }
     while (num);
   }
-  catch (std::invalid_argument const& e1)
+  catch (std::logic_error const& e)
   {
-    std::cout << e1.what() << "\n";
+    std::cout << e.what() << "\n";
     return 1;
   }
-  catch (std::logic_error const& e2)
-  {
-    std::cout << e2.what() << "\n";
-    return 1;
-  }
-  std::cout << max << "\n";
+  std::cout << sequenceEven() << "\n";
   return 0;
 }
