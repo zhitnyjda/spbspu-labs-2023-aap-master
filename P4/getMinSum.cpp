933e66cc8e1152ec3d9e5f389792e816587b453f
teca14 <telleskarbalio.a@edu.spbstu.ru>
2023-11-13 00:02:21 +0300

    first try

---
+++ P4/getMinSum.cpp
@@ -0,0 +1,34 @@
+#include "duplicateRows.hpp"
+#include <stdexcept>
+#include <limits>
+
+size_t getMinSum(const size_t * arr, size_t m, size_t n)
+{
+  if (m == 0 || n == 0)
+    return 0;
+
+  if (m != n)
+    throw std::invalid_argument("La matriz debe ser cuadrada");
+
+  size_t minSum = arr[0]; // Inicializamos con el primer elemento
+
+  for (size_t i = 1; i < m; ++i)
+  {
+    size_t sum = 0;
+    for (size_t j = 0; j <= i; ++j)
+      sum += arr[i * n + j] + arr[j * n + i];
+    if (sum < minSum)
+      minSum = sum;
+  }
+
+  for (size_t j = 1; j < n; ++j)
+  {
+    size_t sum = 0;
+    for (size_t i = 0; i < m && i + j < n; ++i)
+      sum += arr[i * n + i + j] + arr[(i + j) * n + i];
+    if (sum < minSum)
+      minSum = sum;
+  }
+
+  return minSum;
+}
---
+++ P4/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+#include <cstddef>
+#include <cstring>
+#include "duplicateRows.hpp"
+int main(int argc, char *argv[])
+{
+  if (argc != 4)
+  {
+    std::cout << "Invalid number of arguments." << '\n';
+    return 1;
+  }
+  size_t rows = 0;
+  size_t columns = 0;
+  std::ifstream file(argv[2]);
+  file >> rows >> columns;
+  if (!file)
+  {
+    std::cout << "Error.";
+    return 1;
+  }
+  if (!std::strcmp(argv[1], "1"))
+  {
+    size_t  static_matrix[10000];
+    for (size_t i = 0; i < rows * columns; i++)
+    {
+      file >> static_matrix[i];
+      if (!file)
+      {
+        std::cout << "Error..";
+        return 1;
+      }
+    }
+    std::ofstream out(argv[3]);
+    out << getMinSum(static_matrix, rows, columns) << "\n";
+  }
+  else if (!std::strcmp(argv[1], "2"))
+  {
+    if (rows != columns)
+    {
+      std::cerr << "Matrix must be square" << '\n';
+      return 1;
+    }
+    size_t * dynamic_matrix = new size_t[rows * columns];
+    for (size_t i = 0; i < rows * columns; i++)
+    {
+      file >> dynamic_matrix[i];
+      if (!file)
+      {
+        std::cout << "Error...";
+        delete[] dynamic_matrix;
+        return 1;
+      }
+    }
+    std::ofstream out(argv[3]);
+    try
+    {
+      size_t minSum = getMinSum(reinterpret_cast<const size_t*>(dynamic_matrix), rows, columns);
+      out << minSum << "\n";
+    }
+    catch (const std::invalid_argument &e)
+    {
+      delete[] dynamic_matrix;
+      std::cout << "Error:\n";
+      std::cout << e.what();
+      return 0;
+    }
+    delete[] dynamic_matrix;
+  }
+  else
+  {
+    std::cout << "No such parameter";
+    return 1;
+  }
+  return 0;
+}
