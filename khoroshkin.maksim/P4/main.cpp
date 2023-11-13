0afe86468dba0072d505ed50bbcd8432aa96639d
Maxim Khoroshkin <khoroskinmaks@gmail.com>
2023-11-13 21:14:34 +0300

    fixed comments again

--- khoroshkin.maksim/P4/main.cpp
+++ khoroshkin.maksim/P4/main.cpp
@@ -4,91 +4,89 @@
 
 int main(int argc, char ** argv)
 {
-  if (argc != 4)
-  {
-    std::cerr << "Error: should contain 4 parameters\n";
-    return 1;
-  }
-
-  int num;
   try
   {
-   num = std::stoll(argv[1]);
+    if (argc != 4)
+    {
+      throw std::invalid_argument( "Error: should contain 4 parameters\n");
+    }
+
+    int num = std::stoll(argv[1]);
+
+    if (num > 2 || num < 1)
+    {
+      throw std::invalid_argument("Error: I can accept only 1 or 2\n");
+    }
+    else if (num == 1)
+    {
+      int statMatrix[1000] = { 0 };
+      khoroshkin::matrix Matrix;
+      std::ifstream inputFile(argv[2]);
+      int Rows, Cols;
+      inputFile >> Rows >> Cols;
+      if (!inputFile)
+      {
+        throw std::logic_error("Cannot read an input.\n");
+      }
+      try
+      {
+        Matrix.inputArray(inputFile, statMatrix, Rows * Cols);
+      }
+      catch(const std::runtime_error & e)
+      {
+        std::cerr << e.what() << '\n';
+        return 2;
+      }
+      inputFile.close();
+      std::ofstream outputFile(argv[3]);
+      if (!outputFile.is_open())
+      {
+        throw std::logic_error("Cannot open an output.\n");
+      }
+      outputFile << Matrix.minSumOfParallelArray(statMatrix, Rows, Cols);
+      return 0;
+    }
+    else
+    {
+      khoroshkin::matrix Matrix;
+      std::ifstream inputFile(argv[2]);
+      int Rows, Cols;
+      inputFile >> Rows >> Cols;
+      if (!inputFile)
+      {
+        throw std::logic_error("Cannot read an input.\n");
+      }
+      int * dynamicMatrix = new int[Rows * Cols];
+      try
+      {
+        Matrix.inputArray(inputFile, dynamicMatrix, Rows * Cols);
+      }
+      catch(const std::runtime_error & e)
+      {
+        std::cerr << e.what() << '\n';
+        delete[] dynamicMatrix;
+        return 2;
+      }
+      inputFile.close();
+      std::ofstream outputFile(argv[3]);
+      if (!outputFile.is_open())
+      {
+        delete[] dynamicMatrix;
+        throw std::logic_error("Cannot open an output.\n");
+      }
+      outputFile << Matrix.minSumOfParallelArray(dynamicMatrix, Rows, Cols);
+      delete[] dynamicMatrix;
+      return 0;
+    }
   }
   catch (const std::invalid_argument & e)
   {
     std::cerr << e.what();
     return 1;
-    }
-
-  if (num > 2 || num < 1)
+  }
+  catch (const std::logic_error & e)
   {
-    std::cerr << "Error: I can accept only 1 or 2\n";
+    std::cerr << e.what();
     return 2;
-  }
-  else if (num == 1)
-  {
-    int statMatrix[1000] = { 0 };
-    khoroshkin::matrix Matrix;
-    int Rows, Cols;
-    std::ifstream inputFile(argv[2]);
-    inputFile >> Rows >> Cols;
-    if (!inputFile)
-    {
-      std::cerr << "Cannot read an input.\n";
-      return 2;
-    }
-    try
-    {
-      Matrix.inputArray(inputFile, statMatrix, Rows * Cols);
-    }
-    catch(const std::runtime_error & e)
-    {
-      std::cerr << e.what() << '\n';
-      return 2;
-    }
-    inputFile.close();
-    std::ofstream outputFile(argv[3]);
-    if (!outputFile.is_open())
-    {
-      std::cerr << "Cannot open an output.\n";
-      return 2;
-    }
-    outputFile << Matrix.minSumOfParallelArray(statMatrix, Rows, Cols);
-    return 0;
-  }
-  else
-  {
-    khoroshkin::matrix Matrix;
-    int Rows, Cols;
-    std::ifstream inputFile(argv[2]);
-    inputFile >> Rows >> Cols;
-    if (!inputFile)
-    {
-      std::cerr << "Cannot read an input.\n";
-      return 2;
-    }
-    int * dynamicMatrix = new int[Rows * Cols];
-    try
-    {
-      Matrix.inputArray(inputFile, dynamicMatrix, Rows * Cols);
-    }
-    catch(const std::runtime_error & e)
-    {
-      std::cerr << e.what() << '\n';
-      delete[] dynamicMatrix;
-      return 2;
-    }
-    inputFile.close();
-    std::ofstream outputFile(argv[3]);
-    if (!outputFile.is_open())
-    {
-      std::cerr << "Cannot open an output.\n";
-      delete[] dynamicMatrix;
-      return 2;
-    }
-    outputFile << Matrix.minSumOfParallelArray(dynamicMatrix, Rows, Cols);
-    delete[] dynamicMatrix;
-    return 0;
   }
 }
