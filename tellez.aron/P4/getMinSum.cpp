4ab663c833c1f4c1373712522d0efd64dd3465d0
teca14 <telleskarbalio.a@edu.spbstu.ru>
2023-11-15 01:27:38 +0300

    fixing something 2.0

--- tellez.aron/P4/getMinSum.cpp
+++ tellez.aron/P4/getMinSum.cpp
@@ -6,12 +6,9 @@ size_t getMinSum(const size_t * arr, size_t m, size_t n)
 {
   if (m == 0 || n == 0)
     return 0;
-
   if (m != n)
-    throw std::invalid_argument("La matriz debe ser cuadrada");
-
-  size_t minSum = arr[0]; // Inicializamos con el primer elemento
-
+    throw std::invalid_argument("matrix must be square");
+  size_t minSum = arr[0];
   for (size_t i = 1; i < m; ++i)
   {
     size_t sum = 0;
@@ -20,7 +17,6 @@ size_t getMinSum(const size_t * arr, size_t m, size_t n)
     if (sum < minSum)
       minSum = sum;
   }
-
   for (size_t j = 1; j < n; ++j)
   {
     size_t sum = 0;
@@ -29,6 +25,5 @@ size_t getMinSum(const size_t * arr, size_t m, size_t n)
     if (sum < minSum)
       minSum = sum;
   }
-
   return minSum;
 }
