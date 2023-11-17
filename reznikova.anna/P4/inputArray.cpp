fda247c626eac62c1b82eddbd701b8912691c759
shavel0k <anna.reznikova@icloud.com>
2023-11-18 02:17:32 +0300

    added dinamic array

---
+++ reznikova.anna/P4/inputArray.cpp
@@ -0,0 +1,14 @@
+#include "inputArray.hpp"
+#include <algorithm>
+#include <fstream>
+
+void readArray::inputArray(std::istream & in, int * m, size_t s, size_t toRead)
+{
+  for (size_t i = 0; i < std::min(toRead, s); ++i)
+  {
+    if (!(in >> m[i]))
+    {
+      throw std::length_error("Can't read an input. \n");
+    }
+  }
+}
--- reznikova.anna/P4/main.cpp
+++ reznikova.anna/P4/main.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <fstream>
 #include "findNumRowLsr.hpp"
+#include "inputArray.hpp"
 
 int main(int argc, char ** argv)
 {
   using namespace matrixStuff;
-
+  using namespace readArray;
+  
   if (argc != 4)
   {
     std::cerr << "Wrong number of arguments.\n";
     return 1;
   }
-
+  
   try
   {
     long long num = std::stoll(argv[1]);
   }
   catch (const std::invalid_argument & e)
   {
-    std::cerr << "Cannot parse a value\n";
+    std::cerr << "Can't parse a value. \n";
     return 1;
   }
   long long num = std::stoll(argv[1]);
-
+  
   size_t rows = 0, cols = 0;
   {
     std::ifstream input(argv[2]);
@@ -30,17 +32,12 @@ int main(int argc, char ** argv)
     input >> cols;
     if (!input)
     {
-      std::cerr << "Cannot read an input\n";
+      std::cerr << "Can't read an input. \n";
       return 2;
     }
   }
-
-  if (num > 2)
-  {
-    std::cerr << "Argument num must be either 1 or 2\n";
-    return 2;
-  }
-  else if (num == 1)
+  
+  if (num == 1)
   {
     int smatrix[10000];
     {
@@ -51,9 +48,7 @@ int main(int argc, char ** argv)
         input >> smatrix[i];
         if (!input)
         {
-          std::cerr << "Cannot read an input.\n";
-          return 2;
-          std::cerr << "Cannot read an input.\n";
+          std::cerr << "Can't read an input.\n";
           return 2;
         }
       }
@@ -63,5 +58,36 @@ int main(int argc, char ** argv)
       output << matrixStuff::findNumRowLsr(smatrix, rows, cols) << "\n";
     }
   }
-return 0;
+  
+  else if (num == 2)
+  {
+    int * dmatrix = new int[rows * cols];
+    {
+      std::ifstream input(argv[2]);
+      input.seekg(3);
+      try
+      {
+        readArray::inputArray(input, dmatrix, rows * cols, rows * cols);
+      }
+      catch(const std::exception & e)
+      {
+        std::cerr << e.what();
+        delete [] dmatrix;
+        return 2;
+      }
+    }
+    {
+      std::ofstream output(argv[3]);
+      output << matrixStuff::findNumRowLsr(dmatrix, rows, cols) << "\n";
+    }
+    delete [] dmatrix;
+  }
+  
+  else
+  {
+    std::cerr << "Argument num must be either 1 or 2. \n";
+    return 2;
+  }
+  
+  return 0;
 }
