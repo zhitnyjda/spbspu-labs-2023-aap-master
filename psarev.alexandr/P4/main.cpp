e37214313bd61ab9e9b43324617d9eaa335c9468
AzZR43L <alexandrpsarev5555@gmail.com>
2023-11-17 22:14:04 +0500

    final changes per comments

--- psarev.alexandr/P4/main.cpp
+++ psarev.alexandr/P4/main.cpp
@@ -20,59 +20,45 @@ int main(int argc, char ** argv)
     return 1;
   }
 
-  if (taskNum == 1) {
-    int stMatrix[10000] = { 0 };
-    psarev::matrix Matrix;
-    std::ifstream input(argv[2]);
-    size_t rows = 0, cols = 0;
-    if (!(input >> rows >> cols)) {
-    std::cerr << "Error: Can't read input file!\n";
-    return 2;
-    }
-
-    size_t counter = 0;
-    if (Matrix.inputMatrix(input, stMatrix, rows * cols, counter) != (rows*cols)) {
-      std::cerr << "Error: Mismatch of dimension and values!\n";
-      return 2;
-    }
-
-    int verdict = Matrix.isTriMatrix(stMatrix, rows, cols);
-    std::ofstream output(argv[3]);
-    if (!output.is_open()) {
-    std::cerr << "Error: Can't open an output file!\n";
-    return 2;
-    }
-    verdict == 1 ? output << "true\n" : output << "false\n";
-
-  } else if (taskNum == 2) {
-    psarev::matrix Matrix;
-    std::ifstream input(argv[2]);
-    size_t rows = 0, cols = 0;
-    if (!(input >> rows >> cols)) {
-    std::cerr << "Error: Can't read input file!\n";
-    return 2;
-    }
-    int* dynMatrix = new int[rows * cols];
-
-    size_t counter = 0;
-    if (Matrix.inputMatrix(input, dynMatrix, rows * cols, counter) != (rows*cols)) {
-      std::cerr << "Error: Mismatch of dimension and values!\n";
-      delete[] dynMatrix;
-      return 2;
-    }
-
-    int verdict = Matrix.isTriMatrix(dynMatrix, rows, cols);
-    std::ofstream output(argv[3]);
-    if (!output.is_open()) {
-    std::cerr << "Error: Can't open an output file!\n";
-    return 2;
-    }
-    verdict == 1 ? output << "true\n" : output << "false\n";
-    delete[] dynMatrix;
-
-  } else {
+  if ((taskNum != 1) && (taskNum != 2)) {
     std::cerr << "Error: Wrong first argument value!\n";
     return 1;
+
+  } else {
+    psarev::matrix Matrix;
+    std::ifstream input(argv[2]);
+    size_t rows = 0, cols = 0;
+    if (!(input >> rows >> cols)) {
+      std::cerr << "Error: Can't read input file!\n";
+      return 2;
+    }
+    std::ofstream output(argv[3]);
+    if (!output.is_open()) {
+      std::cerr << "Error: Can't open an output file!\n";
+      return 2;
+    }
+    size_t counter = 0;
+
+    if (taskNum == 1) {
+      int stMatrix[10000] = { 0 };
+      if (Matrix.inputMatrix(input, stMatrix, rows*cols, counter) != (rows*cols)) {
+        std::cerr << "Error: Mismatch of dimension and values!\n";
+        return 2;
+      }
+      int verdict = Matrix.isTriMatrix(stMatrix, rows, cols);
+      verdict == 1 ? output << "true\n" : output << "false\n";
+
+    } else if (taskNum == 2) {
+      int* dynMatrix = new int[rows * cols];
+      if (Matrix.inputMatrix(input, dynMatrix, rows*cols, counter) != (rows*cols)) {
+        std::cerr << "Error: Mismatch of dimension and values!\n";
+        delete[] dynMatrix;
+        return 2;
+      }
+      int verdict = Matrix.isTriMatrix(dynMatrix, rows, cols);
+      verdict == 1 ? output << "true\n" : output << "false\n";
+      delete[] dynMatrix;
+    }
   }
 
   return 0;
