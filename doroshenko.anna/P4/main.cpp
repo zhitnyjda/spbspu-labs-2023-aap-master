879a384b1e22221c847dbb033afb1e8dfbd43cd0
anyadg <anna_giraffe@mail.ru>
2023-11-19 22:22:28 +0300

    Trying to fix build and acceptance errors

--- doroshenko.anna/P4/main.cpp
+++ doroshenko.anna/P4/main.cpp
@@ -13,41 +13,39 @@ int main(int argc, char** argv)
     size_t rows_;
     size_t cols_;
     size_t result;
-    std::ifstream input;
-    input.open(argv[2]);
-    if (!input)
     {
-      std::cerr << "File not open\n";
-      return 2;
+      std::ifstream input(argv[2]);
+      if (!input)
+      {
+        std::cerr << "File not open\n";
+        return 2;
+      }
+      input >> rows_ >> cols_;
+      if (!input)
+      {
+        std::cerr << "It is not number\n";
+        return 2;
+      }
+      if (rows_ < 3 || cols_ < 3)
+      {
+        std::cerr << "Not enough elements\n";
+        return 2;
+      }
+      if (num == 1)
+      {
+        int staticMatrix[] = { 0 };
+        Matrix.inputArray(input, staticMatrix, rows_ * cols_);
+        result = Matrix.findingLocMax(staticMatrix, rows_, cols_);
+      }
+      else
+      {
+        int* dynMatrix = new int[rows_ * cols_];
+        Matrix.inputArray(input, dynMatrix, rows_ * cols_);
+        result = Matrix.findingLocMax(dynMatrix, rows_, cols_);
+        delete[] dynMatrix;
+      }
     }
-    input >> rows_ >> cols_;
-    if (!input)
-    {
-      std::cerr << "It is not number\n";
-      return 2;
-    }
-    if (rows_ < 3 && cols_ < 3)
-    {
-      std::cerr << "Not enough elements\n";
-      return 2;
-    }
-    if (num == 1)
-    {
-      int* staticMatrix[] = { 0 };
-      Matrix.inputArray(input, staticMatrix, rows_ * cols_);
-      result = Matrix.findingLocMax(staticMatrix, rows_, cols_);
-      input.close();
-    }
-    else
-    {
-      int* dynMatrix = new int[rows_ * cols_];
-      Matrix.inputArray(input, dynMatrix, rows_ * cols_);
-      result = Matrix.findingLocMax(dynMatrix, rows_, cols_);
-      input.close();
-      delete[] dynMatrix;
-    }
-    std::ofstream output;
-    output.open(argv[3]);
+    std::ofstream output(argv[3]);
     if (!output) {
       std::cerr << "File not open\n";
       return 2;
