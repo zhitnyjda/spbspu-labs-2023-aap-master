dd4bdb9dc9fb53e7995ba292e9911b481068c76b
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-11-20 01:32:36 +0300

    Update helperFunc.h

--- zhitnyj.dmitriy/P1/SUM-DUP/helperFunc.h
+++ zhitnyj.dmitriy/P1/SUM-DUP/helperFunc.h
@@ -5,7 +5,16 @@
 struct helperFunc
 {
 public:
-  helperFunc(int count = 0, int n = 0, int l_ = 0, int s_ = 0) { helperFunc::count = count, helperFunc::n = n; helperFunc::l_ = l_; helperFunc::s = s_; }
+  helperFunc(int count = 0, int n = 0, int l_ = 0, int s_ = 0)
+  { 
+    helperFunc::count = count;
+    
+    helperFunc::n = n;
+    
+    helperFunc::l_ = l_;
+    
+    helperFunc::s = s_; 
+  }
   int operator()(std::istream *str);
   int operator()(int n);
 private:
