5194890931a4825efbe09ecf02ac2a4d99655007
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-11-20 01:23:08 +0300

    Update helperFunc.cpp

--- zhitnyj.dmitriy/P1/EQL-SEQ/helperFunc.cpp
+++ zhitnyj.dmitriy/P1/EQL-SEQ/helperFunc.cpp
@@ -4,56 +4,56 @@
 
 int helperFunc::operator()(std::istream *str)
 {
-	int n_ = 0;
+  int n_ = 0;
 
-	do
-	{
-		try
-		{
-			if(str->peek() != 10 && helperFunc::n == n_)
-			{
-				std::cout << "Sequence should end with 0!" << std::endl;
+  do
+  {
+    try
+    {
+      if(str->peek() != 10 && helperFunc::n == n_)
+      {
+        std::cout << "Sequence should end with 0!" << std::endl;
 
-				return 1;
-			}
-			
-			*str >> n_;
+        return 1;
+      }
 
-			if (str->fail())
-			{
-				std::cout << "Not a sequence!" << std::endl;
+      *str >> n_;
 
-				return 2;
-			}
-			helperFunc::operator()(n_);
-		}
-		catch(std::exception &err)
-		{
-			err.what();
-		}
+      if (str->fail())
+      {
+        std::cout << "Not a sequence!" << std::endl;
 
-	}while(n_);
+        return 2;
+      }
+      helperFunc::operator()(n_);
+    }
+    catch(std::exception &err)
+    {
+      err.what();
+    }
 
-	std::cout << "Count: " << ( helperFunc::mCount + 1 ) << std::endl;
+  }while(n_);
 
-	return 0;
+  std::cout << "Count: " << ( helperFunc::mCount + 1 ) << std::endl;
+
+  return 0;
 }
 
 int helperFunc::operator()(int n_)
 {
-	if (n_ == helperFunc::n)
-	{
-		helperFunc::count++;
+  if (n_ == helperFunc::n)
+  {
+    helperFunc::count++;
 
-		if(helperFunc::count > helperFunc::mCount)
-			helperFunc::mCount = helperFunc::count;
-	}
-	else
-	{
-		helperFunc::n = n_;
+    if(helperFunc::count > helperFunc::mCount)
+      helperFunc::mCount = helperFunc::count;
+  }
+  else
+  {
+    helperFunc::n = n_;
 
-		helperFunc::count = 0;
-	}
+    helperFunc::count = 0;
+  }
 
-	return 0;
+  return 0;
 }
