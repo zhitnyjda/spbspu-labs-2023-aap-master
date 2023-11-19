8243c067b5d692d10f25bd0f5c045f3cf51568a6
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-11-20 02:39:30 +0300

    Update helpersFunc.cpp

--- zhitnyj.dmitriy/P1/SUM-DUP/helpersFunc.cpp
+++ zhitnyj.dmitriy/P1/SUM-DUP/helpersFunc.cpp
@@ -2,64 +2,64 @@
 #include <stdexcept>
 #include "helpersFunc.h"
 
-int helperFunc::operator()(std::istream *str)
+int helperFunc::operator()(std::istream* str)
 {
-  int n_ = 0;
+   int n_ = 0;
 
-  do
-  {
-    try
-    {
-      if(str->peek() == 10 && helperFunc::n == n_)
+   do
+   {
+      try
       {
-        std::cout << "Sequence should end with 0!" << std::endl;
+         if (str->peek() == 10 && helperFunc::n == n_)
+         {
+            std::cout << "Sequence should end with 0!" << std::endl;
 
-        return 1;
+            return 1;
+         }
+         *str >> n_;
+
+         if (str->fail())
+         {
+            std::cout << "Not a sequence!" << std::endl;
+
+            return 2;
+         }
+
+         helperFunc::operator()(n_);
       }
-      *str >> n_;
-
-      if (str->fail())
+      catch (std::exception& err)
       {
-        std::cout << "Not a sequence!" << std::endl;
-
-        return 2;
+         err.what();
       }
+      l_++;
+   } while (n_);
 
-      helperFunc::operator()(n_);
-    }
-    catch(std::exception &err)
-    {
-      err.what();
-    }
-    l_++;
-  }while(n_);
+   if (l_ < 4)
+   {
+      std::cout << "Too short sequence!" << std::endl;
 
-  if (l_ < 4)
-  {
-    std::cout << "Too short sequence!" << std::endl;
+      return 1;
+   }
 
-    return 1;
-  }
+   std::cout << "Count: " << helperFunc::count << std::endl;
 
-  std::cout << "Count: " << helperFunc::count << std::endl;
-
-  return 0;
+   return 0;
 }
 
 int helperFunc::operator()(int n_)
 {
-  if (!((l_ + 1) % 3) && (l_ != 0))
-  {
-	  if((s ==  n_))
-		  helperFunc::count++;
+   if (!(( l_ + 1 ) % 3 ) && ( l_ != 0 ))
+   {
+      if (( s == n_ ))
+         helperFunc::count++;
 
-    helperFunc::s = 0;
-  }
-  else
-  {
-	  helperFunc::s += n_;
-	  helperFunc::n = n_;
-  }
+      helperFunc::s = 0;
+   }
+   else
+   {
+      helperFunc::s += n_;
+      helperFunc::n = n_;
+   }
 
-  return 0;
+   return 0;
 }
