de82b141cb039f416c21b8d2b65d9589fe514f48
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-11-20 01:38:43 +0300

    Update helpersFunc.cpp

--- zhitnyj.dmitriy/P1/SUM-DUP/helpersFunc.cpp
+++ zhitnyj.dmitriy/P1/SUM-DUP/helpersFunc.cpp
@@ -4,62 +4,62 @@
 
 int helperFunc::operator()(std::istream *str)
 {
-	int n_ = 0;
+  int n_ = 0;
 
-	do
-	{
-		try
-		{
-			if(str->peek() == 10 && helperFunc::n == n_)
-			{
-				std::cout << "Sequence should end with 0!" << std::endl;
+  do
+  {
+    try
+    {
+      if(str->peek() == 10 && helperFunc::n == n_)
+      {
+        std::cout << "Sequence should end with 0!" << std::endl;
 
-				return 1;
-			}
-			*str >> n_;
+        return 1;
+      }
+      *str >> n_;
 
-			if (str->fail())
-			{
-				std::cout << "Not a sequence!" << std::endl;
+      if (str->fail())
+      {
+        std::cout << "Not a sequence!" << std::endl;
 
-				return 2;
-			}
+        return 2;
+      }
 
-			helperFunc::operator()(n_);
-		}
-		catch(std::exception &err)
-		{
-			err.what();
-		}
-		l_++;
-	}while(n_);
+      helperFunc::operator()(n_);
+    }
+    catch(std::exception &err)
+    {
+      err.what();
+    }
+    l_++;
+  }while(n_);
 
-	if (l_ < 4)
-	{
-		std::cout << "Too short sequence!" << std::endl;
+  if (l_ < 4)
+  {
+    std::cout << "Too short sequence!" << std::endl;
 
-		return 1;
-	}
+    return 1;
+  }
 
-	std::cout << "Count: " << helperFunc::count << std::endl;
+  std::cout << "Count: " << helperFunc::count << std::endl;
 
-	return 0;
+  return 0;
 }
 
 int helperFunc::operator()(int n_)
 {
-	helperFunc::n = n_;
+  helperFunc::n = n_;
 
-	if (((n_ + s) == helperFunc::n) && !(l_ % 3) && (l_ != 0))
-	{
-		helperFunc::count++;
+  if (((n_ + s) == helperFunc::n) && !(l_ % 3) && (l_ != 0))
+  {
+    helperFunc::count++;
 
-		helperFunc::s = 0;
-	}
-	else
-	{
-		helperFunc::n = n_;
-	}
+    helperFunc::s = 0;
+  }
+  else
+  {
+    helperFunc::n = n_;
+  }
 
-	return 0;
+  return 0;
 }
