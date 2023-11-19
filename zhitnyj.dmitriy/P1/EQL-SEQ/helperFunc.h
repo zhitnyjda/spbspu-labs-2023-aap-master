2e7c13147442eaddaf4801eadffcfeedeff2406d
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-11-20 00:11:33 +0300

    Create helperFunc.h

---
+++ zhitnyj.dmitriy/P1/EQL-SEQ/helperFunc.h
@@ -0,0 +1,15 @@
+#ifndef SOURCES_1_HELPERSFUNC_H
+#define SOURCES_1_HELPERSFUNC_H
+#include <iostream>
+
+struct helperFunc
+{
+public:
+	helperFunc(int count = 0, int mCount = 0, int n = 0) { helperFunc::count = count, helperFunc::mCount = mCount; helperFunc::n = n; }
+	int operator()(std::istream *str);
+	int operator()(int n);
+private:
+	int count, mCount, n;
+};
+
+#endif
