6beac1c11192544e9667997e2dbfa562a7d9fb1e
anyadg <anna_giraffe@mail.ru>
2023-11-20 00:41:55 +0300

    Detailed description of the algorythmfor the two types of arrays

--- doroshenko.anna/P4/main.cpp
+++ doroshenko.anna/P4/main.cpp
@@ -9,58 +9,93 @@ int main(int argc, char** argv)
   try
   {
     long long num = readingArguments(argc, argv);
-    Matrix Matrix;
-    size_t rows_;
-    size_t cols_;
-    size_t result;
+  }
+  catch(std::logic_error& firstError)
+  {
+    std::cerr << firstError.what();
+    return 1;
+  }
+  Matrix Matrix;
+  size_t rows_;
+  size_t cols_;
+  long long num = readingArguments(argc, argv);
+  if (num == 1)
+  {
+    std::ifstream input(argv[2]);
+    if (!input)
     {
-      std::ifstream input(argv[2]);
-      if (!input)
-      {
-        std::cerr << "File not open\n";
-        return 2;
-      }
-      input >> rows_ >> cols_;
-      if (!input)
-      {
-        std::cerr << "It is not number\n";
-        return 2;
-      }
-      if (rows_ < 3 || cols_ < 3)
-      {
-        std::cerr << "Not enough elements\n";
-        return 2;
-      }
-      if (num == 1)
-      {
-        int staticMatrix[] = { 0 };
-        Matrix.inputArray(input, staticMatrix, rows_ * cols_);
-        result = Matrix.findingLocMax(staticMatrix, rows_, cols_);
-      }
-      else
-      {
-        int* dynMatrix = new int[rows_ * cols_];
-        Matrix.inputArray(input, dynMatrix, rows_ * cols_);
-        result = Matrix.findingLocMax(dynMatrix, rows_, cols_);
-        delete[] dynMatrix;
-      }
-    }
-    std::ofstream output(argv[3]);
-    if (!output) {
       std::cerr << "File not open\n";
       return 2;
     }
-    output << result;
+    input >> rows_ >> cols_;
+    if (!input)
+    {
+      std::cerr << "It is not number\n";
+      return 2;
+    }
+    if (rows_ < 3 || cols_ < 3)
+    {
+      std::cerr << "Not enough elements\n";
+      return 2;
+    }
+    int staticMatrix[] = { 0 };
+    try
+    {
+      Matrix.inputArray(input, staticMatrix, rows_ * cols_);
+    }
+    catch (std::invalid_argument& secondError)
+    {
+      std::cerr << secondError.what();
+      return 2;
+    }
+    Matrix.inputArray(input, staticMatrix, rows_ * cols_);
+    std::ofstream output(argv[3]);
+    if (!output)
+    {
+      std::cerr << "File not open\n";
+      return 2;
+    }
+    output << Matrix.findingLocMax(staticMatrix, rows_, cols_);
     return 0;
   }
-  catch (const std::invalid_argument& firstError)
+  else
   {
-    std::cout << firstError.what() << "\n";
-    return 2;
-  }
-  catch (const std::logic_error& secondError)
-  {
-    std::cout << secondError.what() << "\n";
-    return 1;
+    std::ifstream input(argv[2]);
+    if (!input)
+    {
+      std::cerr << "File not open\n";
+      return 2;
+    }
+    input >> rows_ >> cols_;
+    if (!input)
+    {
+      std::cerr << "It is not number\n";
+      return 2;
+    }
+    if (rows_ < 3 || cols_ < 3)
+    {
+      std::cerr << "Not enough elements\n";
+      return 2;
+    }
+    int* dynMatrix = new int[rows_ * cols_];
+    try
+    {
+      Matrix.inputArray(input, dynMatrix, rows_ * cols_);
+    }
+    catch (std::invalid_argument& secondError)
+    {
+      std::cerr << secondError.what();
+      return 2;
+    }
+    Matrix.inputArray(input, dynMatrix, rows_ * cols_);
+    std::ofstream output(argv[3]);
+    if (!output)
+    {
+      std::cerr << "File not open\n";
+      return 2;
+    }
+    output << Matrix.findingLocMax(dynMatrix, rows_, cols_);
+    delete[] dynMatrix;
+    return 0;
   }
 }
