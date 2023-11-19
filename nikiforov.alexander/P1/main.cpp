d644fba1f1ea417846da8687d81e3ae4a4840993
AlexNik <sahan@DESKTOP-R8OTJOJ>
2023-11-19 13:54:16 +0300

    adding an error of too short sequence

--- nikiforov.alexander/P1/main.cpp
+++ nikiforov.alexander/P1/main.cpp
@@ -12,12 +12,18 @@ int main()
     const int min_count = std::numeric_limits< int >::min();
     int num = 0;
     int beforenum = min_count;
+    size_t lenth = 0;
     Sequence arrMin;
     do
     {
       std::cin >> num;
       arrMin(std::cin, beforenum, num);
+      lenth++;
     } while (num != 0);
+
+    if (lenth == 1) {
+      throw std::overflow_error("Too short sequence");
+    }
 
     size_t result = arrMin.printSequence();
     std::cout << result;
@@ -28,7 +34,7 @@ int main()
   }
   catch (const std::overflow_error& e)
   {
-    std::cout << e.what();
+    std::cout << e.what() << "\n";
     return 2;
   }
 return 0;
