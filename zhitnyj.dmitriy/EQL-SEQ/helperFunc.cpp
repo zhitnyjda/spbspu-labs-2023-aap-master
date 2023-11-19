6b9cfa1bb4942749cb97ab35b8e5b72d1f98e5f8
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-11-20 00:10:13 +0300

    Add files via upload

---
+++ zhitnyj.dmitriy/EQL-SEQ/helperFunc.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdexcept>
+#include "helpersFunc.h"
+
+int helperFunc::operator()(std::istream *str)
+{
+	int n_ = 0;
+
+	do
+	{
+		try
+		{
+			*str >> n_;
+
+			if (str->fail())
+			{
+				std::cout << "Not a sequence!" << std::endl;
+
+				return 2;
+			}
+			helperFunc::operator()(n_);
+		}
+		catch(std::exception &err)
+		{
+			err.what();
+		}
+
+	}while(n_);
+
+	std::cout << "Count: " << ( helperFunc::mCount + 1 ) << std::endl;
+
+	return 0;
+}
+
+int helperFunc::operator()(int n_)
+{
+	n_ == helperFunc::n ? (helperFunc::count++, (helperFunc::count > helperFunc::mCount ? helperFunc::mCount = helperFunc::count : helperFunc::count)) : (helperFunc::n = n_, helperFunc::count = 0);
+
+	return 0;
+}
