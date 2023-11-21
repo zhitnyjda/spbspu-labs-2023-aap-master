832bc0879f018adb1bd6926f42193dbb4c5907a5
shavel0k <anna.reznikova@icloud.com>
2023-11-21 15:14:02 +0300

    added string input and output

--- reznikova.anna/P3/main.cpp
+++ reznikova.anna/P3/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 int main() {
-  
+
+  char chr = 0;
+  size_t read = 0;
+
+  char * input = new char[10];
+
+  std::cin >> std::noskipws;
+
+  while ((std::cin >> chr) && (chr != '\n'))
+  {
+    input[read++] = chr;
+  }
+  std::cin >> std::skipws;
+
+  std::cout << input << "\n";
+  delete [] input;
   return 0;
 }
