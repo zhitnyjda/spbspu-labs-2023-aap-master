1b143c9ae43dafe66d7368505bba1e9ef974dd2c
ponomarev.viacheslav <slava_ponomarev_05@mail.ru>
2023-11-22 22:28:39 +0300

    SeqCounter struct is implemented

--- ponomarev.viacheslav/P1/main.cpp
+++ ponomarev.viacheslav/P1/main.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 #include <limits>
+#include <stdexcept>
+
+struct SequenceCounter
+{
+public:
+  SequenceCounter():
+    seqLength_(0)
+  {}
+
+  void count(int number)
+  {
+    const size_t maxSize = std::numeric_limits< size_t >::max();
+    if (seqLength_ == maxSize)
+    {
+      throw std::logic_error("Sequence is too long.");
+    }
+    ++seqLength_;
+  }
+
+  size_t getResult() const
+  {
+    return seqLength_;
+  }
+
+private:
+  size_t seqLength_;
+};
 
 int main()
 {
   int number = 0;
-  size_t seqLength = 0;
+  SequenceCounter sequenceCounter;
   do
-  {  
+  {
     std::cin >> number;
     if (!std::cin)
     {
@@ -15,15 +42,16 @@ int main()
     }
     else if (number != 0)
     {
-      const size_t maxSize = std::numeric_limits< size_t >::max();
-      if (seqLength == maxSize)
+      try
       {
-        std::cerr << "Sequence is too long.\n";
-        return 2;
+        sequenceCounter.count(number);
       }
-      ++seqLength;
+      catch (const std::exception & e)
+      {
+        std::cerr << "Error: " << e.what() << "\n";
+      }
     }
   }
   while (number != 0);
-  std::cout << seqLength << "\n";
+  std::cout << sequenceCounter.getResult() << "\n";
 }
