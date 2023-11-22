d6a892cfbde83ad116fd43901f6b0de3d8812412
0412erik <litsinger00@bk.ru>
2023-11-22 21:26:24 +0300

    fix build

--- litsinger.ernest/P4/function.cpp
+++ litsinger.ernest/P4/function.cpp
@@ -5,27 +5,6 @@
 #include <limits>
 namespace litsinger
 {
-  std::ifstream input;
-  std::ofstream output;
-  int fillArguments(int args, const char* argv[])
-  {
-    if (args > 4)
-    {
-      throw (std::invalid_argument("Too many arguments\n"));
-    }
-    if (args < 4)
-    {
-      throw (std::invalid_argument("Not enough arguments\n"));
-    }
-    input.open(argv[2]);
-    output.open(argv[3]);
-    int n = std::strtoll(argv[1], nullptr, 10);
-    if (n < 1 || n>2)
-    {
-      throw(std::invalid_argument("First parameter is out of range\n"));
-    }
-    return n;
-  }
   int MaximalSum(const int * arr, size_t rows, size_t cols)
   {
     int maxSum = arr[0];
--- litsinger.ernest/P4/function.hpp
+++ litsinger.ernest/P4/function.hpp
@@ -3,7 +3,6 @@
 #include <cstddef>
 namespace litsinger
 {
-  int fillArguments(int, const char* []);
   int MaximalSum(const int * arr, size_t rows, size_t cols);
 }
 #endif
