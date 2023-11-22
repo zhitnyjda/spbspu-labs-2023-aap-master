ba8aa4e4f3f4753868d802a3c9944660cc3880c7
0412erik <litsinger00@bk.ru>
2023-11-22 21:08:05 +0300

    valgrind fix

--- litsinger.ernest/P4/main.cpp
+++ litsinger.ernest/P4/main.cpp
@@ -4,92 +4,83 @@
 #include "function.hpp"
 
 using namespace litsinger;
-int main(int args, const char* argv[])
+int main(int args,char * argv[])
 {
-  std::ifstream input(argv[2]);
-  std::ofstream output(argv[3]);
-  try
+  if (args < 4)
   {
-    int task = litsinger::fillArguments(args, argv);
-    if (task == 1)
-    {
-      std::ifstream input(argv[2]);
-      size_t rows = 0;
-      size_t cols = 0;
-      input >> rows;
-      input >> cols;
-      if (!input.is_open())
-      {
-        std::cerr << "Error\n";
-        return 1;
-      }
-      if (rows != cols)
-      {
-        std::cerr << "Matrix is not a square\n";
-        return 1;
-      }
-      int static_array[10000] = {0};
-      for (size_t i = 0; i < (rows * cols); i++)
-      {
-        input >> static_array[i];
-        if (!input)
-        {
-          std::cerr << "Input error\n";
-          return 1;
-        }
-      }
-      if (!output.is_open())
-      {
-        std::cerr << "Can not open output file\n";
-        return 1;
-      }
-      output << MaximalSum(static_array, rows, cols);
-    }
-    else if (task == 2)
-    {
-      size_t rows = 0;
-      size_t cols = 0;
-      input >> rows;
-      input >> cols;
-      if (!input.is_open())
-      {
-        std::cerr << "Error\n";
-        return 1;
-      }
-      if (rows != cols)
-      {
-        std::cerr << "Matrix is not a square\n";
-        return 1;
-      }
-      int * dynamic_array = new int[rows * cols];
-      for (size_t i = 0; i < (rows * cols); i++)
-      {
-        input >> dynamic_array[i];
-        if (!input)
-        {
-          std::cerr << "Input error\n";
-          return 1;
-          delete[] dynamic_array;
-        }
-      }
-      if (!output.is_open())
-      {
-        std::cerr << "Can not open output file\n";
-        delete[] dynamic_array;
-      }
-      output << MaximalSum(reinterpret_cast<const int*>(dynamic_array), rows, cols);
-      delete[] dynamic_array;
-    }
-    else if (task != 1 && task != 2)
-    {
-      std::cerr << "Error\n";
-      return 1;
-    }
-    return 0;
-  }
-  catch (std::invalid_argument& e)
-  {
-    std::cerr << e.what();
+    std::cerr << "Not enough arguments\n";
     return 1;
   }
+  if (args > 4)
+  {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+  size_t rows = 0;
+  size_t cols = 0;
+  std::ifstream input(argv[2]);
+  input >> rows;
+  input >> cols;
+  if (!input)
+  {
+    std::cerr << "Error\n";
+    return 1;
+  }
+  if (!std::strcmp(argv[1], "1"))
+  {
+    if (rows != cols)
+    {
+      std::cerr << "Matrix is not a square\n";
+      return 0;
+    }
+    int static_array[10000] = {0};
+    for (size_t i = 0; i < (rows * cols); i++)
+    {
+      input >> static_array[i];
+      if (!input)
+      {
+        std::cerr << "Input error\n";
+        return 1;
+      }
+    }
+    std::ofstream output(argv[3]);
+    output << MaximalSum(static_array, rows, cols) << "\n";
+  }
+  else if (!std::strcmp(argv[1], "2"))
+  {
+    if (rows != cols)
+    {
+      std::cerr << "Matrix is not a square\n";
+      return 0;
+    }
+    int * dynamic_array = new int[rows * cols];
+    for (size_t i = 0; i < (rows * cols); i++)
+    {
+      input >> dynamic_array[i];
+      if (!input)
+      {
+        std::cerr << "Input error\n";
+        delete[] dynamic_array;
+        return 1;
+      }
+    }
+    std::ofstream output(argv[3]);
+    try
+    {
+      if (rows != cols)
+      {
+        std::cerr << "Matrix is not a square\n";
+        delete[] dynamic_array;
+        return 0;
+      }
+      output << MaximalSum(reinterpret_cast<const int*>(dynamic_array), rows, cols);
+    }
+    delete[] dynamic_array;
+  }
+  else if (task != 1 && task != 2)
+  {
+    std::cerr << "Error\n";
+    return 1;
+  }
+  return 0;
 }
