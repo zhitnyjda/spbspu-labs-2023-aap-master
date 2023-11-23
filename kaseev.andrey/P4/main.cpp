8bba9ffafce4049cf5f08ade38fdaf500c226c49
herznaetkcto <147743642+herznaetkcto@users.noreply.github.com>
2023-11-24 00:08:13 +0300

    отчаяние

--- kaseev.andrey/P4/main.cpp
+++ kaseev.andrey/P4/main.cpp
@@ -4,35 +4,36 @@
 
 int main(int argc, char **argv)
 {
-  if (argc > 4)
-  {
-    throw (std::invalid_argument("There are more arguments than should be!"));
-  }
-  if (argc < 4)
-  {
-    throw (std::invalid_argument("There are not enough arguments!"));
-  }
-  int num = std::strtoll(argv[1], nullptr, 10);
-  if (num != 1 && num != 2)
-  {
-    throw (std::invalid_argument("Invalid argument!"));
-  }
-  std::ifstream input(argv[2]);
-  int m = 0, n = 0;
-  if (!(input >> n >> m))
-  {
-    std::cerr << "invalid input" << "\n";
-    return 1;
-  }
-  if (m <= 0 || n <= 0)
-  {
-    std::cerr << "Invalid matrix dimensions." << "\n";
-    return 1;
-  }
-  std::ofstream output(argv[3]);
-  int size = n * m;
   try
   {
+    if (argc > 4)
+    {
+      throw (std::invalid_argument("There are more arguments than should be!"));
+    }
+    if (argc < 4)
+    {
+      throw (std::invalid_argument("There are not enough arguments!"));
+    }
+    int num = std::strtoll(argv[1], nullptr, 10);
+    if (num != 1 && num != 2)
+    {
+      throw (std::invalid_argument("Invalid argument!"));
+    }
+    std::ifstream input(argv[2]);
+    int m = 0, n = 0;
+    if (!(input >> n >> m))
+    {
+      std::cerr << "invalid input" << "\n";
+      return 1;
+    }
+    if (m <= 0 || n <= 0)
+    {
+      std::cerr << "Invalid matrix dimensions." << "\n";
+      return 1;
+    }
+
+  std::ofstream output(argv[3]);
+  int size = n * m;
     if (num == 1)
     {
       int *matrix = new int[n * m];
