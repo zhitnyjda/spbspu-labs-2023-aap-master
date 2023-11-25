1d4b88a3e60796f7b67a1d96b018325ec1e53612
Maxim Khoroshkin <khoroskinmaks@gmail.com>
2023-11-25 18:14:45 +0300

    checking

--- khoroshkin.maksim/P4/main.cpp
+++ khoroshkin.maksim/P4/main.cpp
@@ -39,8 +39,6 @@ int main(int argc, char ** argv)
           std::cerr << "Error: was filled only " << successOrNO+1 << " elements";
           return 2;
         }
-      }
-      {
         std::ofstream outputFile(argv[3]);
         khoroshkin::fillingOutputFile(outputFile, statMatrix, Rows, Cols);
       }
@@ -51,22 +49,18 @@ int main(int argc, char ** argv)
       int Rows, Cols;
       try
       {
+        std::ifstream inputFile(argv[2]);
+        khoroshkin::fillingRowsAndCols(inputFile, Rows, Cols);
+        dynamicMatrix = new int[Rows * Cols];
+        int successOrNO = khoroshkin::inputArray(inputFile, dynamicMatrix, Rows * Cols);
+        if (successOrNO != Rows*Cols)
         {
-          std::ifstream inputFile(argv[2]);
-          khoroshkin::fillingRowsAndCols(inputFile, Rows, Cols);
-          dynamicMatrix = new int[Rows * Cols];
-          int successOrNO = khoroshkin::inputArray(inputFile, dynamicMatrix, Rows * Cols);
-          if (successOrNO != Rows*Cols)
-          {
-            std::cerr << "Error: was filled only " << successOrNO+1 << " elements";
-            delete[] dynamicMatrix;
-            return 2;
-          }
+          std::cerr << "Error: was filled only " << successOrNO+1 << " elements";
+          delete[] dynamicMatrix;
+          return 2;
         }
-        {
-          std::ofstream outputFile(argv[3]);
-          khoroshkin::fillingOutputFile(outputFile, dynamicMatrix, Rows, Cols);
-        }
+        std::ofstream outputFile(argv[3]);
+        khoroshkin::fillingOutputFile(outputFile, dynamicMatrix, Rows, Cols);
       }
       catch (const std::logic_error & e)
       {
@@ -74,7 +68,7 @@ int main(int argc, char ** argv)
         delete[] dynamicMatrix;
         return 2;
       }
-    delete[] dynamicMatrix;
+      delete[] dynamicMatrix;
     }
   }
   catch (const std::logic_error & e)
