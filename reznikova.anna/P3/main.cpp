4ec383088be3527d2759c6a3ab6fa66933b0f099
shavel0k <anna.reznikova@icloud.com>
2023-11-25 22:01:28 +0300

    now second string is specified inside the program

--- reznikova.anna/P3/main.cpp
+++ reznikova.anna/P3/main.cpp
@@ -5,9 +5,9 @@
 int main() {
 
   size_t first_size = 20;
-  size_t second_size = 20;
+  size_t second_size = 3;
   size_t add_size = 20;
-  
+
   char * first_string = nullptr;
   try
   {
@@ -19,27 +19,15 @@ int main() {
     std::cerr << e.what() << "\n";
     return 1;
   }
-  
-  char * second_string = nullptr;
-  try
-  {
-    second_string = inputString(std::cin, second_size, add_size);
-  }
-  catch (const std::exception & e)
-  {
-    delete [] second_string;
-    std::cerr << e.what() << "\n";
-    return 1;
-  }
-  
+
+  char second_string[3] = {'a', 'b', ' '};
   char * result_string = nullptr;
   result_string = ExcludeSecString(first_string, second_string, first_size, second_size);
-  
+
   std::cout << result_string << "\n";
-  
+
   delete [] first_string;
-  delete [] second_string;
   delete [] result_string;
-  
+
   return 0;
 }
