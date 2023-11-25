9f1f3edf177cf079d2ebd4027a64fb624ebf9079
Maxim Khoroshkin <khoroskinmaks@gmail.com>
2023-11-25 18:02:34 +0300

    trying to delete dynamicMatrix

--- khoroshkin.maksim/P4/main.cpp
+++ khoroshkin.maksim/P4/main.cpp
@@ -39,8 +39,6 @@ int main(int argc, char ** argv)
           std::cerr << "Error: was filled only " << successOrNO+1 << " elements";
           return 2;
         }
-      }
-      {
         std::ofstream outputFile(argv[3]);
         khoroshkin::fillingOutputFile(outputFile, statMatrix, Rows, Cols);
       }
@@ -48,25 +46,22 @@ int main(int argc, char ** argv)
     else
     {
       int * dynamicMatrix;
-      int Rows, Cols;
       try
       {
+        int Rows, Cols;
+        std::ifstream inputFile(argv[2]);
+        khoroshkin::fillingRowsAndCols(inputFile, Rows, Cols);
+        dynamicMatrix = new int[Rows * Cols];
+        int successOrNO = khoroshkin::inputArray(inputFile, dynamicMatrix, Rows * Cols);
+        if (successOrNO != Rows*Cols)
         {
-          std::ifstream inputFile(argv[2]);
-          khoroshkin::fillingRowsAndCols(inputFile, Rows, Cols);
-          dynamicMatrix = new int[Rows * Cols];
-          int successOrNO = khoroshkin::inputArray(inputFile, dynamicMatrix, Rows * Cols);
-          if (successOrNO != Rows*Cols)
-          {
-            std::cerr << "Error: was filled only " << successOrNO+1 << " elements";
-            delete[] dynamicMatrix;
-            return 2;
-          }
+          std::cerr << "Error: was filled only " << successOrNO+1 << " elements";
+          delete[] dynamicMatrix;
+          return 2;
         }
-        {
-          std::ofstream outputFile(argv[3]);
-          khoroshkin::fillingOutputFile(outputFile, dynamicMatrix, Rows, Cols);
-        }
+        std::ofstream outputFile(argv[3]);
+        khoroshkin::fillingOutputFile(outputFile, dynamicMatrix, Rows, Cols);
+        delete[] dynamicMatrix;
       }
       catch (const std::logic_error & e)
       {
