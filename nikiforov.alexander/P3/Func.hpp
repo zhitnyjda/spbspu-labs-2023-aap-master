139ded5a6a9df8525d7427abac6de6a261e1f827
AlexNik <sahan@DESKTOP-R8OTJOJ>
2023-11-26 22:50:51 +0300

    first ver P3....

---
+++ nikiforov.alexander/P3/Func.hpp
@@ -0,0 +1,13 @@
+#ifndef FUNC_HPP
+#define FUNC_HPP
+
+namespace nikiforov {
+	class Str
+	{
+	public:
+		void AddElem(char*& arr, size_t& size, size_t& lenth);
+		int result(char* str1, char* str2, size_t len1, size_t len2);
+	private:
+	};
+}
+#endif
