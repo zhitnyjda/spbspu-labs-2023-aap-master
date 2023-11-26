aade7d89a813f194bff520a3dc80cb83c3a58405
harduserNIKITA <cumpotttt@yandex.ru>
2023-11-26 16:04:12 +0300

    add private in struct

--- kovshikov.nikita/P4/count_saddle.hpp
+++ kovshikov.nikita/P4/count_saddle.hpp
@@ -7,12 +7,14 @@ namespace kovshikov
 {
   struct Saddle
   {
-    Saddle();
-    size_t operator()(size_t rows, size_t cols, size_t * matrix);
-    size_t count;
-    size_t minrows;
-    size_t maxcols;
-    size_t coordinateMinRowsj;
+    public:
+      Saddle();
+      size_t operator()(size_t rows, size_t cols, size_t * matrix);
+    private:
+      size_t count;
+      size_t minrows;
+      size_t maxcols;
+      size_t coordinateMinRowsj;
   };
 }
 
