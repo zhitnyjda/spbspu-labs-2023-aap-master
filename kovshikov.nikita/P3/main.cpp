b749111cfea639f2c97a84933e7162fc3713f76d
harduserNIKITA <cumpotttt@yandex.ru>
2023-11-26 21:51:25 +0300

    the code executes its own version

--- kovshikov.nikita/P3/main.cpp
+++ kovshikov.nikita/P3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <cctype>
 int main()
 {
   size_t read = 0;
@@ -32,9 +33,27 @@ int main()
       }
     }
   }
-
-
-
+  char output[26] = {};
+  std::cout << output << "\n";
+  int element = 0;
+  bool flag = false;
+  for(int i = 97; i <= 122 ; i++)
+  {
+    for(size_t j = 0; j < read; j++)
+    {
+      if(char(i) == (char)tolower(array[j]))
+      {
+        flag = true;
+      }
+      else if((j == read - 1) && char(i) != (char)tolower(array[j]) && flag == false)
+      {
+        output[element++] = char(i);
+      }
+    }
+    flag = false;
+  }
+  std::cout << output << "\n";
+  std::cin >> std::skipws;
   delete[] array;
   return 0;
 }
