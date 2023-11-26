56fbc16699e3841f4de5e7e2f0cdaae4d352ec9c
herznaetkcto <147743642+herznaetkcto@users.noreply.github.com>
2023-11-26 18:15:40 +0300

    первый блин

---
+++ kaseev.andrey/P3/string.cpp
@@ -0,0 +1,43 @@
+#include "string.hpp"
+#include "iostream"
+#include <cstring>
+#include <algorithm>
+
+char *kaseev::readLine()
+{
+  std::string inputStr;
+  std::getline(std::cin, inputStr);
+  char *charArray = new char[inputStr.length() + 1];
+  strcpy(charArray, inputStr.c_str());
+
+  return charArray;
+}
+
+char* kaseev::interleaveStrings(char* str1, char* str2) {
+  if (str1 == nullptr || str2 == nullptr) {
+    return nullptr;
+  }
+  size_t len1 = strlen(str1);
+  size_t len2 = strlen(str2);
+  size_t newLen = len1 + len2 + 1;
+  size_t t = 0;
+  if (newLen <= 1) {
+    return nullptr;
+  }
+  char* newStr = new (std::nothrow) char[newLen];
+  if (newStr == nullptr) {
+    return nullptr;
+  }
+  for (size_t i = 0; i < std::min(len1, len2); ++i) {
+    newStr[t++] = str1[i];
+    newStr[t++] = str2[i];
+  }
+  if (len1 < len2) {
+    strcpy(newStr + t, str2 + len1);
+  } else if (len1 > len2) {
+    strcpy(newStr + t, str1 + len2);
+  } else {
+    newStr[t] = '\0';
+  }
+  return newStr;
+}
---
+++ kaseev.andrey/P3/string.hpp
@@ -0,0 +1,10 @@
+#ifndef STRING_HPP
+#define STRING_HPP
+#include <cstddef>
+
+namespace kaseev
+{
+  char* readLine();
+  char* interleaveStrings(char* str1, char* str2);
+}
+#endif
