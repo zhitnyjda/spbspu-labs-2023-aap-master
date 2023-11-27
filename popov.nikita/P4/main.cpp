accd19db41941a514ce7868066f5af0e87e1b6e6
pinniks <nik.nwlife@gmail.com>
2023-11-27 16:29:31 +0300

    fix

--- popov.nikita/P4/main.cpp
+++ popov.nikita/P4/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include "maximumSumStat.hpp"
 
 int main(int argc, char ** argv)
 {
@@ -16,8 +17,14 @@ int main(int argc, char ** argv)
   int sum = 0;
   std::ifstream input(argv[2]);
   input >> rows >> cols;
+  if (rows == 0 and cols == 0)
+  {
+    return 0;
+  }
   if (num == 1)
   {
+    using namespace popov;
+    maxSumStat maxsSumStat;
     int a[10000] = {0};
     for (int i = 1; i <= rows * cols; ++i)
     {
@@ -25,57 +32,8 @@ int main(int argc, char ** argv)
       input >> value;
       a[i] = value;
     }
-    for (int j = 1; j <= 2; ++j)
-    {
-      sum = 0;
-      if (j == 1)
-      {
-        for (int i = 1; i < cols; ++i)
-        {
-          sum = 0;
-          sum = sum + a[i];
-          for (int q = 1; q < i; ++q)
-          {
-            sum = sum + a[i + (q * (rows - 1))];
-          }
-          if ((sum > max) and (i * j != 1))
-          {
-            max = sum;
-          }
-          if (i * j == 1)
-          {
-            if (max < a[j * i])
-            {
-              max = a[j * i];
-            }
-          }
-        }
-      }
-      if (j == 2)
-      {
-        int count = rows;
-        for (int w = rows * 2;w <= rows * cols;w + rows)
-        {
-          sum = 0;
-          sum = a[w];
-          count = count - 1;
-          for (int e = 1; e < count; ++e)
-          {
-            sum = sum + a[w + (e * (rows - 1))];
-          }
-          if ((sum > max) and (w != rows * cols))
-          {
-            max = sum;
-          }
-          if (w == rows * cols)
-          {
-            if (max < a[w])
-            {
-              max = a[w];
-            }
-          }
-        }
-      }
-    }
+    maxsSumStat(a, &rows, &cols, &sum, &max);
+    std::ofstream out(argv[3]);
+    out << max;
   }
 }
