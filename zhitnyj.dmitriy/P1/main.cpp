7c9c0a0d6f50e289b41a2375a0820afac7ff3a89
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-11-29 00:11:23 +0300

    Update main.cpp

--- zhitnyj.dmitriy/P1/main.cpp
+++ zhitnyj.dmitriy/P1/main.cpp
@@ -5,58 +5,29 @@
 int main()
 {
   int elm = 0;
-  int var = 0;
-  int varFlag = 0; // you shoud change flag state to select a variant, this made because of acceptence error
 
-  if (varFlag) {
-    std::cin >> var;
+  zhitnyj::funcEqlSeq funcEqlSeq;
+  zhitnyj::funcSumDup funcSumDup;
+
+  try {
+    if (!(std::cin >> elm)) {
+      throw std::logic_error("Invalid input");
+    }
+    while (elm) {
+      funcEqlSeq(elm);
+      funcSumDup(elm);
+      if (!(std::cin >> elm)) {
+        throw std::logic_error("Invalid input");
+      }
+    }
+    std::cout << funcEqlSeq() << "\n";
+    std::cout << funcSumDup() << "\n";
+  } catch (const std::length_error &exc) {
+    std::cerr << exc.what() << "\n";
+    return 2;
   }
-
-  if (var == 0) {
-    zhitnyj::funcEqlSeq funcEqlSeq;
-    try {
-      if (!(std::cin >> elm)) {
-        throw std::logic_error("Invalid input");
-      }
-      while (elm) {
-        funcEqlSeq(elm);
-        if (!(std::cin >> elm)) {
-          throw std::logic_error("Invalid input");
-        }
-      }
-      std::cout << funcEqlSeq() << "\n";
-    } catch (const std::length_error &exc) {
-      std::cerr << exc.what() << "\n";
-      return 2;
-    }
-    catch (const std::logic_error &exc) {
-      std::cerr << exc.what() << "\n";
-      return 1;
-    }
-  } else if (var == 1) {
-    zhitnyj::funcSumDup funcSumDup;
-
-    try {
-      if (!(std::cin >> elm)) {
-        throw std::logic_error("Invalid input");
-      }
-      while (elm) {
-        funcSumDup(elm);
-        if (!(std::cin >> elm)) {
-          throw std::logic_error("Invalid input");
-        }
-      }
-      std::cout << funcSumDup() << "\n";
-    } catch (const std::length_error &exc) {
-      std::cerr << exc.what() << "\n";
-      return 2;
-    }
-    catch (const std::logic_error &exc) {
-      std::cerr << exc.what() << "\n";
-      return 1;
-    }
-  } else {
-    std::cerr << "Invalid var\n";
+  catch (const std::logic_error &exc) {
+    std::cerr << exc.what() << "\n";
     return 1;
   }
 
