e7a5a8b14ade2c8cf87db51fe11f87d196343bdd
SeryjAlexey <slekseyvs.2005@gmail.com>
2023-11-28 19:10:34 +0300

    last fixes

--- seryj.alexey/P4/main.cpp
+++ seryj.alexey/P4/main.cpp
@@ -8,11 +8,14 @@ int main(int args, const char* argv[])
   {
     n = std::stoi(argv[1], nullptr, 10);
     if(argv[1][1])
-      throw std::logic_error("");
+    {
+      std::cerr << "First argument is wrong\n";
+      return 1;
+    }
   }
   catch (std::logic_error const& e)
   {
-    std::cerr << "First argument is wrong\n";
+    std::cerr << "First argument is not a number\n";
     return 1;
   }
   if (args != 4 || !(n == 2 || n == 1))
@@ -34,17 +37,18 @@ int main(int args, const char* argv[])
   }
   size_t to_read = line * column;
   const size_t max_size = 10000;
-  int static_array[max_size] = { 0 };
-  int* dynamic_array = new int[to_read];
-  int * pointer = (n == 1) ? static_array : dynamic_array;
-  size_t result = seryj::fillArray((n==1) ? max_size : to_read, to_read, input, pointer);
+  int static_values [max_size] = { 0 };
+  int * values = (n == 1) ? static_values : new int[to_read];
+  size_t result = seryj::fillArray((n==1) ? max_size : to_read, to_read, input, values);
   if (result != to_read)
   {
     std::cerr << "Could only read " << result << " numbers. I needed " << to_read << "\n";
-    delete[] dynamic_array;
+    if(n == 2)
+      delete[] values;
     return 1;
   }
-  seryj::printAvgOfNeigbours(line, column, output, pointer);
-  delete[] dynamic_array;
+  seryj::printAvgOfNeigbours(line, column, output, values);
+  if(n == 2)
+    delete[] values;
   return 0;
 }
