9470dbcde77cc202d49daa0b036b96933be20dcf
oksidmargantsa <sukachva.maria@mail.ru>
2023-11-29 23:17:39 +0300

    fixing bugs in acceptance

--- sukacheva.maria/P4/main.cpp
+++ sukacheva.maria/P4/main.cpp
@@ -25,44 +25,41 @@ int main(int argc, char** argv)
 
   size_t rows = 0, cols = 0;
   std::ifstream input(argv[2]);
-  try
+  input >> rows >> cols;
+  if (!input)
   {
-    input >> rows;
-    input >> cols;
-  }
-  catch (const std::invalid_argument& e)
-  {
-    std::cerr << "Cannot read an input.\n";
-    return 2;
+    std::cerr << "Uable to read file.\n";
   }
   if (cols == 0 || rows == 0)
   {
     return 0;
   }
-  size_t sizeOfMatrix = rows * cols;
-  int staticMatrix[10000];
-  int* resultMatrix = (num == 1) ? staticMatrix : new int[sizeOfMatrix];
-  size_t result = inputMatrix(input, resultMatrix, sizeOfMatrix, sizeOfMatrix);
-  if (!(result == sizeOfMatrix))
+  if (num == 1 || num == 2)
   {
-    std::cerr << "Cannot read an array. Read " << result << " elements.\n";
+    size_t sizeOfMatrix = rows * cols;
+    int staticMatrix[10000];
+    int* resultMatrix = (num == 1) ? staticMatrix : new int[sizeOfMatrix];
+    size_t result = inputMatrix(input, resultMatrix, sizeOfMatrix, sizeOfMatrix);
+    if (!(result == sizeOfMatrix))
+    {
+      std::cerr << "Cannot read an array. Read " << result << " elements.\n";
+      if (num == 2)
+      {
+        delete[] resultMatrix;
+      }
+      return 2;
+    }
+    std::ofstream output(argv[3]);
+    output << MaxSideDiagonal(resultMatrix, rows) << "\n" << upperTriangularMatrix(resultMatrix, cols, rows);
     if (num == 2)
     {
       delete[] resultMatrix;
     }
-    return 2;
-  }
-
-  if (num == 2)
-  {
-    delete[] resultMatrix;
   }
   else
   {
     std::cerr << "Argument is out of range. \n";
     return 1;
   }
-  std::ofstream output(argv[3]);
-  output << MaxSideDiagonal(resultMatrix, rows) << "\n" << upperTriangularMatrix(resultMatrix, cols, rows);
   return 0;
 }
