0f84d286efcbab052ead4335221f8955b70c36c2
SeryjAlexey <slekseyvs.2005@gmail.com>
2023-11-30 00:01:57 +0300

    подправил названия под cg в мэйне

--- seryj.alexey/P4/main.cpp
+++ seryj.alexey/P4/main.cpp
@@ -3,10 +3,10 @@
 #include "Funcs.hpp"
 int main(int args, const char* argv[])
 {
-  int n = 0;
+  int task = 0;
   try
   {
-    n = std::stoi(argv[1], nullptr, 10);
+    task = std::stoi(argv[1], nullptr, 10);
     if(argv[1][1])
     {
       std::cerr << "First argument is wrong\n";
@@ -18,7 +18,7 @@ int main(int args, const char* argv[])
     std::cerr << "First argument is not a number\n";
     return 1;
   }
-  if (args != 4 || !(n == 2 || n == 1))
+  if (args != 4 || !(task == 2 || task == 1))
   {
    std::cerr << "Invalid arguments\n";
    return 1;
@@ -37,18 +37,18 @@ int main(int args, const char* argv[])
   }
   size_t to_read = line * column;
   const size_t max_size = 10000;
-  int integer_array1 [max_size] = { 0 };
-  int * values = (n == 1) ? integer_array1 : new int[to_read];
-  size_t result = seryj::fillArray((n==1) ? max_size : to_read, to_read, input, values);
+  int first_task_array [max_size] = { 0 };
+  int * values = (task == 1) ? first_task_array : new int[to_read];
+  size_t result = seryj::fillArray ((task == 1) ? max_size : to_read, to_read, input, values);
   if (result != to_read)
   {
     std::cerr << "Could only read " << result << " numbers. I needed " << to_read << "\n";
-    if(n == 2)
+    if (task == 2)
       delete[] values;
     return 1;
   }
   seryj::printAvgOfNeigbours(line, column, output, values);
-  if(n == 2)
+  if (task == 2)
     delete[] values;
   return 0;
 }
