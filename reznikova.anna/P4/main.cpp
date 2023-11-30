f00fd5e30d0f315e69d3b4e4577a9583509b7c50
shavel0k <anna.reznikova@icloud.com>
2023-11-30 23:11:08 +0300

    removed code duplication and fixed some other comments

--- reznikova.anna/P4/main.cpp
+++ reznikova.anna/P4/main.cpp
@@ -6,22 +6,24 @@
 int main(int argc, char ** argv)
 {
   using namespace reznikova;
-
   if (argc != 4)
   {
     std::cerr << "Wrong number of arguments.\n";
     return 1;
   }
-
   char * end = nullptr;
   long long num = 0;
-  num = std::strtoll(argv[1], &end, 10);
+  num = std::strtoll(argv[1], std::addressof(end), 10);
   if (*end != '\0')
   {
     std::cerr << "Can't parse a value.\n";
     return 1;
   }
-
+  else if (num < 1 || num > 2)
+  {
+    std::cerr << "Argument num must be either 1 or 2. \n";
+    return 1;
+  }
   size_t rows = 0, cols = 0;
   {
     std::ifstream input(argv[2]);
@@ -32,47 +34,28 @@ int main(int argc, char ** argv)
       std::cerr << "Can't read an input.\n";
       return 2;
     }
-
+    size_t capacity = rows * cols;
     size_t result = 0;
-    if (num == 1)
+    int smatrix[10000];
+    int * matrix = (num == 1) ? smatrix : new int[capacity];
+    size_t readed = 0;
+    readed = reznikova::inputArray(input, matrix, capacity, capacity);
+    if (readed != (capacity))
     {
-      int smatrix[10000];
-      for (size_t i = 0; i < rows * cols; i++)
-      {
-        input >> smatrix[i];
-        if (!input)
-        {
-          std::cerr << "Wrong input. Readed only " << i << " out of " << (rows * cols) << "\n";
-          return 2;
-        }
-      }
-      result = reznikova::findNumRowLsr(smatrix, rows, cols);
+     std::cerr << "Wrong input. Readed only " << readed << " out of " << (capacity) << "\n";
+     if (num == 2)
+     {
+       delete [] matrix;
+     }
+     return 2;
     }
-
-    else if (num == 2)
+    result = reznikova::findNumRowLsr(matrix, rows, cols);
+    if (num == 2)
     {
-      int * dmatrix = new int[rows * cols];
-      size_t readed = 0;
-      readed = reznikova::inputArray(input, dmatrix, rows * cols, rows * cols);
-      if (readed != (rows * cols))
-      {
-        std::cerr << "Wrong input. Readed only " << readed << " out of " << (rows * cols) << "\n";
-        delete [] dmatrix;
-        return 2;
-      }
-      result = reznikova::findNumRowLsr(dmatrix, rows, cols);
-      delete [] dmatrix;
+      delete [] matrix;
     }
-
-    else
-    {
-      std::cerr << "Argument num must be either 1 or 2. \n";
-      return 2;
-    }
-
     std::ofstream output(argv[3]);
     output << result << "\n";
   }
-
   return 0;
 }
