3a5988c7ea06e81a24b1798115bfe99a1aba562d
AzZR43L <alexandrpsarev5555@gmail.com>
2023-12-01 00:38:26 +0500

    trying to resolve conflict by myself

---
+++ psarev.alexandr/P4/checkMatrix.hpp
@@ -0,0 +1,9 @@
+#ifndef CHECK_MATRIX_HPP
+#define CHECK_MATRIX_HPP
+
+namespace psarev
+{
+  bool isTriMatrix(int* matrix, int rows, int cols);
+}
+#endif
+
---
+++ psarev.alexandr/P4/inputMatrix.hpp
@@ -0,0 +1,11 @@
+#ifndef INPUT_MATRIX_HPP
+#define INPUT_MATRIX_HPP
+#include <iosfwd>
+
+namespace psarev
+{
+  size_t inputMatrix(std::istream& in, int* matrix, size_t cap);
+}
+
+#endif
+
