963507b7b72869d3cbd3415903919dab3e827bae
urtanto <anikanovvlad@yandex.ru>
2023-12-02 23:08:22 +0300

    solve RMV-VOW

---
+++ anikanov.vladislav/P3/RMVVOW.cpp
@@ -0,0 +1,24 @@
+#include "RMVVOW.h"
+#include <cctype>
+
+bool isVowel(char c) {
+  c = std::tolower(c);
+  return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
+
+char *dropVowels(char *string, size_t len){
+  char *new_string = new char[len];
+  size_t j = 0;
+  for (int i = 0; i < strlen(string) + 1; ++i) {
+    if (!isVowel(string[i])) {
+      new_string[j++] = string[i];
+    }
+  }
+  delete[] string;
+  string = new char[len];
+  for (int i = 0; i < len; ++i) {
+    string[i] = new_string[i];
+  }
+  delete[] new_string;
+  return string;
+}
---
+++ anikanov.vladislav/P3/RMVVOW.h
@@ -0,0 +1,8 @@
+#ifndef RMVVOW_H
+#define RMVVOW_H
+
+bool isVowel(char);
+
+char *dropVowels(char *, size_t);
+
+#endif
