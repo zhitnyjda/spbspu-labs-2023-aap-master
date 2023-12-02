e8b3d4a8484ec2562a90006393657919327376fa
teca14 <telleskarbalio.a@edu.spbstu.ru>
2023-12-02 16:51:22 +0300

    fixing valgrind

--- tellez.aron/P4/main.cpp
+++ tellez.aron/P4/main.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <vector>
 #include <stdexcept>
-#include <cstring>
 #include "getMinSum.hpp"
 #include "readMatrix.hpp"
 
@@ -13,53 +12,58 @@ int main(int argc, char* argv[])
     std::cerr << "Invalid number of arguments." << '\n';
     return 1;
   }
-  size_t* matrix = nullptr;
+  size_t rows = 0;
+  size_t columns = 0;
+  std::ifstream file(argv[2]);
+  file >> rows >> columns;
+  if (!file)
+  {
+    std::cerr << "Error reading matrix dimensions.\n";
+    return 1;
+  }
+  size_t * matrix = nullptr;
   try
   {
     size_t num = std::stoi(argv[1]);
-    if (num != 1 && num != 2)
+    if (num == 1 || num == 2)
     {
-      throw std::invalid_argument("Invalid parameter. Use 1 or 2.");
-    }
-    size_t rows = 0;
-    size_t columns = 0;
-    std::ifstream file(argv[2]);
-    file >> rows >> columns;
-    if (!file)
-    {
-      std::cerr << "Error: Unable to read matrix dimensions from file...\n";
-      return 1;
-    }
-    std::vector<size_t> static_matrix(10000);
-    if (num == 1)
-    {
-      matrix = static_matrix.data();
+      size_t size_of_matrix = rows * columns;
+      std::vector<size_t> static_matrix(10000);
+      if (num == 1)
+      {
+        matrix = static_matrix.data();
+      }
+      else
+      {
+        matrix = new size_t[size_of_matrix];
+      }
+      size_t result = readMatrix(file, matrix, size_of_matrix, size_of_matrix);
+      if (!(result == size_of_matrix))
+      {
+        std::cerr << "Cannot read an array. Read " << result << " elements.\n";
+        if (num == 2)
+        {
+          delete[] matrix;
+        }
+        return 2;
+      }
+      std::ofstream out(argv[3]);
+      size_t min_sum = getMinSum(matrix, rows, columns);
+      out << min_sum << "\n";
+      if (num == 2)
+      {
+        delete[] matrix;
+      }
     }
     else
     {
-      matrix = new size_t[rows * columns];
-    }
-    size_t result = readMatrix(file, matrix, rows, columns);
-    if (result != rows * columns)
-    {
-      std::cerr << "Can't read the whole array check the elements \n";
-      return 2;
-    }
-    std::ofstream out(argv[3]);
-    size_t min_sum = getMinSum(matrix, rows, columns);
-    out << min_sum << "\n";
-    if (num == 2)
-    {
-      delete[] matrix;
+      throw std::invalid_argument("No such parameter");
     }
   }
   catch (const std::invalid_argument& e)
   {
     std::cerr << "Error:\n" << e.what() << '\n';
-    if (matrix != nullptr &&  std::strcmp(argv[1], "2") == 0)
-    {
-      delete[] matrix;
-    }
+    delete[] matrix;
     return 1;
   }
   return 0;
