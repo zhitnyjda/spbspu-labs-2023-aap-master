24fd1591ab19ad30f8305c12581c0bd9f38a86bf
ponomarev.viacheslav <slava_ponomarev_05@mail.ru>
2023-12-03 13:55:34 +0300

    Transformation input str to int with strtoll

--- ponomarev.viacheslav/P2/main.cpp
+++ ponomarev.viacheslav/P2/main.cpp
@@ -8,6 +8,21 @@ int main(int argc, char ** argv)
     std::cerr << "I feel that something wrong-_-.\n";
     return 1;
   }
+  char * endOfParcing = nullptr;
+  int num = std::strtoll(argv[1], &endOfParcing, 10);
+  if (num == 0 && endOfParcing == argv[1])
+  {
+    std::cerr << "Cannot parse a value.\n";
+    return 1;
+  }
+  else if (num == 0 && endOfParcing != argv[1])
+  {
+    std::cout << "Num is zero.\n";
+  }
+  else
+  {
+    std::cout << num << "\n";
+  }
   int i = 0;
   {
     std::ifstream input(argv[2]);
