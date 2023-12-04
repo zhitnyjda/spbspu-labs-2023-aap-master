93f1f1103d866a3caa48cfc24986405001d9030c
phohlova <pv.hohlova@edu.spbstu.ru>
2023-12-04 15:44:50 +0300

    p4 main

--- hohlova.polina/P4/main.cpp
+++ hohlova.polina/P4/main.cpp
@@ -7,7 +7,7 @@
 #include <stdexcept>
 
 int main(int argc, char** argv)
-{
+{ 
   using namespace hohlova;
   using namespace hohlovaa;
   if (argc != 4)
@@ -15,44 +15,53 @@ int main(int argc, char** argv)
     std::cerr << "Something wrong, problem with number of arguments\n";
     return 1;
   }
-  size_t rows = 0;
-  size_t cols = 0;
   int num = std::stoll(argv[1]);
-  int* matrix = new int[rows * cols];
-  if (num > 2 || num < 1)
+  int staticm[1000] = { 0 };
+  int* dynamicm = nullptr;
+  try
   {
-    std::cerr << "First parameter is out of range\n";
-    return 1;
-  }
-  std::ifstream inputFile(argv[2]);
-  inputFile >> rows >> cols;
-  if (!inputFile)
-  {
-    std::cerr << "Cannot read an input\n";
-    return 2;
-  }
-  size_t toRead = hohlova::inputArray(inputFile, matrix, rows * cols);
-  if (toRead != rows * cols)
-  {
-    std::cerr << "Cannot read arguments\n";
-    if (num == 2)
+    int rows = 0;
+    int cols = 0;
+    std::ifstream input(argv[2]);
+    input >> rows >> cols;
+    if (!input)
     {
-      delete[] matrix;
+      std::cerr << "Cannot read an input\n";
+      return 2;
     }
-    return 2;
+    std::ofstream output(argv[3]);
+    if (num > 2 || num < 1)
+    {
+      std::cerr << "It can be only 1 or 2\n";
+      return 2;
+    }
+    else if (num == 1)
+    {
+      output << hohlovaa::countStrings(staticm, rows, cols);
+    }
+    else
+    {
+      int* dynamicm = new int[rows * cols];
+      size_t toRead = hohlova::inputArray(input, dynamicm, rows * cols);
+      if (toRead != rows * cols)
+      {
+        std::cerr << "Not all elements are read\n";
+        delete[] dynamicm;
+      }
+      std::ofstream output(argv[3]);
+      if (!output)
+      {
+        std::cerr << "File not open\n";
+        delete[] dynamicm;
+        return 2;
+      }
+      output << hohlovaa::countStrings(dynamicm, rows, cols);
+    }
   }
-  std::ofstream output(argv[3]);
-  if (!output)
+  catch (const std::logic_error& e)
   {
-    std::cerr << "Cannot read an input file\n";
+    std::cerr << e.what();
     return 2;
-  }
-  output << hohlovaa::countStrings(matrix, rows, cols);
-  if (num == 2)
-  {
-    delete[] matrix;
   }
   return 0;
 }
-
-
