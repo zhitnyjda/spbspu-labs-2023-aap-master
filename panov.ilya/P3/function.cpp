622d7c3f6885cfa6f11e43912c87c2a72c6d95c6
ilyapanov123 <89517673116g@gmail.com>
2023-12-06 16:44:25 +0300

    P3 fix

--- panov.ilya/P3/function.cpp
+++ panov.ilya/P3/function.cpp
@@ -6,14 +6,11 @@ std::string removeVowels(const std::string& input) {
   if (input.empty()) {
     return "";
   }
-
   std::string result;
-
   for (char c : input) {
     if (!std::isalpha(c) || std::strchr("AOEIUYaoeiuy", c) == nullptr) {
       result += c;
     }
   }
-
   return result;
 }
