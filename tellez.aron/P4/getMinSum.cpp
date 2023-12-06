05e02de2395b0fb1ae92d680bed9284192665b5d
teca14 <telleskarbalio.a@edu.spbstu.ru>
2023-12-07 00:36:07 +0300

    fixing valgrind errrors

--- tellez.aron/P4/getMinSum.cpp
+++ tellez.aron/P4/getMinSum.cpp
@@ -1,30 +1,33 @@
 #include "getMinSum.hpp"
 #include <stdexcept>
 
-size_t calculateSum(const size_t* arr, size_t m, size_t n, size_t startRow, size_t startCol)
+size_t calculateSum(const size_t *arr, size_t n, size_t i, size_t j)
 {
-  size_t sum = 0;
-  for (size_t i = 0; i < m && i + startCol < n; ++i)
+  if (i <= j)
   {
-    sum += arr[(i + startRow) * n + i + startCol] + arr[(i + startCol) * n + i + startRow];
+    return arr[i * n + j] + ((i + j < n - 1) ? arr[i * n + n - 1 - j] : 0);
   }
-  return sum;
+  else
+  {
+    return arr[j * n + i] + ((i + j < n - 1) ? arr[j * n + n - 1 - i] : 0);
+  }
 }
 
-size_t getMinSum(const size_t* arr, size_t m, size_t n)
+size_t getMinSum(const size_t *arr, size_t m, size_t n)
 {
   if (m == 0 || n == 0)
     return 0;
   if (m != n)
-  throw std::invalid_argument("matrix must be square");
+    throw std::invalid_argument("La matriz debe ser cuadrada");
   size_t minSum = arr[0];
-  for (size_t i = 1; i < m + n - 1; ++i)
+  for (size_t i = 0; i < m; ++i)
   {
-    size_t row = (i < m) ? i : m - 1;
-    size_t col = (i < m) ? 0 : i - m + 1;
-    size_t sum = calculateSum(arr, m, n, row, col);
-    if (sum < minSum)
-      minSum = sum;
+    for (size_t j = 0; j < n; ++j)
+    {
+      size_t sum = calculateSum(arr, n, i, j);
+      if (sum < minSum)
+        minSum = sum;
+    }
   }
   return minSum;
 }
