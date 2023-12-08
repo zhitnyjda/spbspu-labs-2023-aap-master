f63ee5c09d558d9fcb73abc6495860505c433d2d
sadofeeeva <klover200015@gmail.com>
2023-12-08 15:02:47 +0700

    fx

--- sadofeva.nastya/P4/mail.cpp
+++ sadofeva.nastya/P4/mail.cpp
@@ -39,7 +39,7 @@ int main(int argc, char** argv)
     std::cerr << "File is empty" << "\n";
     return 2;
   }
-  if (rows == 0 and cols == 0)
+  if (rows <= 0 || cols <= 0)
   {
     std::ofstream outp(argv[3]);
     outp << 0;
