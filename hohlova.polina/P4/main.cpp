58a613659f95064a97cd45368b0e90cf0f72b404
phohlova <pv.hohlova@edu.spbstu.ru>
2023-12-09 01:16:48 +0300

    -

--- hohlova.polina/P4/main.cpp
+++ hohlova.polina/P4/main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char** argv)
     std::ofstream output(argv[3]);
     int staticm[1000] = { 0 };
     size_t sizeM = hohlova::inputArray(input, staticm, rows * cols);
-    if (sizeM != rows * cols
+    if (sizeM != rows * cols)
     {
       std::cerr << "Not all elements are read\n";
       return 1;
