947696c463d5ee17e81eacee7d9eb94b474a4dcd
sadofeeeva <klover200015@gmail.com>
2023-12-09 23:32:27 +0700

    пробелы

--- sadofeva.nastya/P3/main.cpp
+++ sadofeva.nastya/P3/main.cpp
@@ -4,11 +4,19 @@
 
 int main()
 {
-    using namespace sadofeva;
-    char* input = new char[100] {};
-    Str string;
+  using namespace sadofeva;
+  char* input = new char[100] {};
+  Str string;
+  try
+  {
     size_t ipt = string.Str_read(input);
     std::cout << string.Hr(input) << "\n";
     delete[] input;
     return 0;
+  }
+  catch (std::logic_error const & e)
+  {
+    std::cerr << e.what() << "\n";
+    return 2;
+  }
 }
