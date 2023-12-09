f9ca3e3e4e21ae3c98bd73ed80ed30819f5e32aa
phohlova <pv.hohlova@edu.spbstu.ru>
2023-12-10 00:05:31 +0300

    ...

--- hohlova.polina/P4/matrix.cpp
+++ hohlova.polina/P4/matrix.cpp
@@ -6,9 +6,9 @@
 size_t hohlovaa::countStrings(const int* matrix, size_t rows, size_t cols)
 {
   size_t counter = 0;
-  for (size_t i = 0; i < rows; i++)
+  for (size_t i = 1; i < rows; i++)
   {
-    for (size_t j = 0; j < cols; j++)
+    for (size_t j = 1; j < cols; j++)
     {
       if (matrix[i + rows * (j - 1)] == matrix[i + rows * j])
       {
