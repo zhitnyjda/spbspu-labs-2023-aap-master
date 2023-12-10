a4155956ecc155664841fc6a82044604ced825e1
shavel0k <anna.reznikova@icloud.com>
2023-12-10 22:46:11 +0300

    trying to fiw acceptance errors

--- reznikova.anna/P3/ExcludeSecString.cpp
+++ reznikova.anna/P3/ExcludeSecString.cpp
@@ -3,11 +3,10 @@
 char * ExcludeSecString(char * first_str, char * sec_str, size_t first_size, size_t sec_size)
 {
     char * result_string = new char[first_size]();
-    int flag = 0;
     size_t read = 0;
     for (size_t i = 0; i < first_size; i++)
     {
-        flag = 0;
+        int flag = 0;
         for (size_t j = 0; j < sec_size; j++)
         {
             if (first_str[i] == sec_str[j])
