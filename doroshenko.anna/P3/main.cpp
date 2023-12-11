d847cc9e23e6bd10500095733c051f5728a86567
anyadg <anna_giraffe@mail.ru>
2023-12-11 19:26:36 +0300

    Added allocation error check and fixed algorithm in main file

--- doroshenko.anna/P3/main.cpp
+++ doroshenko.anna/P3/main.cpp
@@ -8,40 +8,59 @@ int main()
   char ch = 0;
   size_t readEl = 0;
   size_t size = 20;
-  char* input = new char [size] {};
-  std::cin >> std::noskipws;
-  while (std::cin >> ch)
+  try
   {
-    if (readEl < size)
+    char* input = new char [size] {};
+    std::cin >> std::noskipws;
+    while (std::cin >> ch)
     {
-      input[readEl++] = ch;
-      if (ch == '\n')
+      if (!std::cin)
       {
-        input[readEl - 1] = 0;
-        break;
+        std::cerr << "Could not read an element of the string\n";
+        return 1;
+      }
+      else if (readEl < size)
+      {
+        input[readEl++] = ch;
+        if (ch == '\n')
+        {
+          input[readEl - 1] = 0;
+          break;
+        }
+      }
+      else
+      {
+        input = doroshenko::resizeArray(input, size, size + 20);
+        size += 20;
+        input[readEl++] = ch;
       }
     }
-    else
+    bool pair = false;
+    for (size_t i = 0; i < readEl - 1; i++)
     {
-      input = doroshenko::resizeArray(input, size, size + 20);
-      size += 20;
-      input[readEl++] = ch;
-    }
-  }
-  for (size_t i = 0; i < readEl - 1; i++)
-  {
-    for (size_t j = i + 1; j < readEl; j++)
-    {
-      if (input[i] == input[j])
+      for (size_t j = i + 1; j < readEl; j++)
+      {
+        if (input[i] == input[j])
+        {
+          pair = true;
+          input[j] = '\0';
+        }
+      }
+      if (pair == true)
       {
         std::cout << input[i];
         input[i] = '\0';
-        input[j] = '\0';
+        pair = false;
       }
     }
+    std::cin >> std::skipws;
+    std::cout << input << '\n';
+    delete[] input;
+    return 0;
   }
-  std::cin >> std::skipws;
-  std::cout << input << '\n';
-  delete[] input;
-  return 0;
+  catch (const std::bad_alloc& e)
+  {
+    std::cerr << "Allocation error\n";
+    return 1;
+  }
 }
