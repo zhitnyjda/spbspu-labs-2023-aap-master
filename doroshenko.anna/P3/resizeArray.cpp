f8be1a7a561d7388f54e27b5eee01df86c407cfd
anyadg <anna_giraffe@mail.ru>
2023-12-11 16:01:24 +0300

    Fixing build errors

--- doroshenko.anna/P3/resizeArray.cpp
+++ doroshenko.anna/P3/resizeArray.cpp
@@ -3,13 +3,13 @@
 char* doroshenko::resizeArray(char* array, size_t size, size_t newSize)
 {
   char* newArray = new char[newSize + 1];
-  size_t i = 0;
-  size_t j = size;
-  for (i; i < size; i++)
+  size_t i;
+  size_t j;
+  for (i = 0; i < size; i++)
   {
     newArray[i] = array[i];
   }
-  for (j; j < newSize; j++)
+  for (j = size; j < newSize; j++)
   {
     newArray[j] = '0';
   }
