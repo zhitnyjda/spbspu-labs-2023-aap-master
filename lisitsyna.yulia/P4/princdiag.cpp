7ee69cda08e12411ee5742bad272c8a2ffb01eaf
unknown <iulia.lis1tsyna@yandex.ru>
2023-12-11 23:59:54 +0300

    fixed size_t

--- lisitsyna.yulia/P4/princdiag.cpp
+++ lisitsyna.yulia/P4/princdiag.cpp
@@ -6,30 +6,37 @@
 
 size_t lisitsyna::principal::input_ar(std::ifstream& in, int* values, size_t sizeMatrix)
 {
-	for (size_t i = 0; i < sizeMatrix; i++)
-	{
-		in >> values[i];
-		if (!in)
-		{
-			return i;
-		}
-	}
-	return sizeMatrix;
+    for (size_t i = 0; i < sizeMatrix; i++)
+    {
+        in >> values[i];
+        if (!in)
+        {
+            return i;
+        }
+    }
+    return sizeMatrix;
 }
-long long lisitsyna::principal::minSum(int rows, int cols, int* values)
+long long lisitsyna::principal::minS(int rows, int cols, int* values)
 {
-	int minS = std::numeric_limits< int >::max();
-	for (int i = 1 - rows; i < rows; i++)
-	{
-		int curSum = 0;
-		for (int j = 0; j < cols; j++)
-		{
-			if ((i + j) >= 0 && (i + j) < rows)
-			{
-				curSum += values[(i + j) * rows + j];
-			}
-		}
-		minS = std::min(curSum, minS);
-	}
-	return minS;
+    int minS = std::numeric_limits< int >::max();
+    for (int i = 1 - rows; i < rows; i++)
+    {
+        int curSum = 0;
+        for (int j = 0; j < cols; j++)
+        {
+            if ((i + j) >= 0 && (i + j) < rows)
+            {
+                curSum += values[(i + j) * rows + j];
+            }
+        }
+        minS = std::min(curSum, minS);
+    }
+    if (rows * cols == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return minS;
+    }
 }
