c0a4a9e5b5c1c11028e486c42042196a6d04e60b
herznaetkcto <147743642+herznaetkcto@users.noreply.github.com>
2023-12-12 02:19:14 +0300

    .

---
+++ kaseev.andrey/P3/main.cpp
@@ -0,0 +1,25 @@
+#include "string.cpp"
+#include <iostream>
+
+int main()
+{
+  char* str1 = kaseev::readLine();
+  char* str2 = kaseev::readLine();
+  try
+  {
+    char *newStr = kaseev::interleaveStrings(str1, str2);
+    if (newStr == nullptr)
+    {
+      delete[] newStr;
+      throw std::logic_error("Invalid input");
+    }
+    std::cout << newStr;
+    delete[] newStr;
+    return 0;
+  }
+  catch (std::exception &err)
+  {
+    std::cerr << err.what() << "\n";
+    return 1;
+  }
+}
