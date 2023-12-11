bc1a459f6a57de7ed768bff55aea13b762221e21
unknown <iulia.lis1tsyna@yandex.ru>
2023-12-11 16:07:21 +0300

    fix

--- lisitsyna.yulia/P4/princdiag.hpp
+++ lisitsyna.yulia/P4/princdiag.hpp
@@ -7,9 +7,8 @@ namespace lisitsyna
     struct principal
     {
     public:
-        void arraay(int& rows, int& cols, std::ifstream& file, int*& values);
-        long long minSum(int rows, int cols, int* values);
-        void input_ar(std::ifstream& in, int* values, size_t sizeMatrix);
+        size_t input_ar(std::ifstream& in, int* values, size_t sizeMatrix);
+	long long minSum(int rows, int cols, int* values);
     private:
         int rows;
         int cols;
