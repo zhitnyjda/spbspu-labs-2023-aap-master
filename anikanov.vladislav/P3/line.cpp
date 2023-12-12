79c8c6c042ccbe180e9b5453f1eceb25e260d5ac
urtanto <anikanovvlad@yandex.ru>
2023-12-12 18:10:49 +0300

    opa opa

--- anikanov.vladislav/P3/line.cpp
+++ anikanov.vladislav/P3/line.cpp
@@ -67,13 +67,15 @@ Line &Line::operator=(const Line &line)
     return *this;
   }
   len = line.len;
-  delete[] data;
+  if (data != nullptr) {
+    delete[] data;
+  }
   try {
     data = new char[len];
     for (size_t i = 0; i < len; ++i) {
       data[i] = line.data[i];
     }
-  } catch (const std::exception &er){
+  } catch (const std::exception &er) {
     throw std::logic_error("memerr");
   }
   return *this;
