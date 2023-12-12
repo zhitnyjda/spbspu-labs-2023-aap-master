2396399f893b9eb85fab13dbb4e052b9483298d0
Arina Redko <arina.redko.05@mail.ru>
2023-12-12 19:41:59 +0300

    removed break

--- redko.arina/P4/countCols.cpp
+++ redko.arina/P4/countCols.cpp
@@ -2,18 +2,22 @@
 
 int redko::countCols(const int * matrix, int rows, int cols)
 {
-  int colsWithRep = 0;
+  int colsWithoutRep = 0;
+  int elemWithoutRep;
   for (int i = 0; i < cols; ++i)
   {
+    elemWithoutRep = 0;
     for (int j = 1; j < rows; ++j)
     {
-      if (matrix[i + cols * (j - 1)] == matrix[i + cols * j])
+      if (matrix[i + cols * (j - 1)] != matrix[i + cols * j])
       {
-        colsWithRep++;
-        break;
+        elemWithoutRep++;
       }
     }
+    if (elemWithoutRep + 1 == rows)
+    {
+      colsWithoutRep++;
+    }
   }
-  int colsWithoutRep = cols - colsWithRep;
   return colsWithoutRep;
 }
