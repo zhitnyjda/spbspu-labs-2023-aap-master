19975bf548ffbc0e09a8b8379f1f5e2d5d6d5460
teca14 <telleskarbalio.a@edu.spbstu.ru>
2023-12-12 18:59:40 +0300

    fixing something

--- tellez.aron/P3/functions.cpp
+++ tellez.aron/P3/functions.cpp
@@ -8,7 +8,14 @@ char* remplaceChar(const char* str, size_t size, char replaceFrom, char replaceT
   char* result = new char[size + 1]();
   for (size_t i = 0; i < size; ++i)
   {
-    result[i] = (str[i] == replaceFrom && str[i] != '\0') ? replaceTo : str[i];
+    if (str[i] == replaceFrom && str[i] != '\0')
+    {
+      result[i] = replaceTo;
+    }
+    else
+    {
+      result[i] = str[i];
+    }
   }
   result[size] = '\0';
   return result;
