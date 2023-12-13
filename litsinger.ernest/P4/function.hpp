0aef23b83b924e699ddf7baf01db8a0a57dcc312
0412erik <litsinger00@bk.ru>
2023-12-13 21:15:41 +0300

    fix

--- litsinger.ernest/P4/function.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTION_HPP
-#define FUNCTION_HPP
-#include <cstddef>
-#include <fstream>
-namespace litsinger
-{
-  size_t inputArray(std::ifstream & input, int * matrix, size_t sizeMatrix);
-}
-#endif
