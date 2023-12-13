f4d64c3882201d7945807e03426a1001b10a113f
sadofeeeva <klover200015@gmail.com>
2023-12-14 01:26:09 +0700

    fx

--- sadofeva.nastya/P4/main.cpp
+++ sadofeva.nastya/P4/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv)
   size_t rows = 0, cols = 0;
   std::ifstream input(argv[2]);
   input >> rows;
-  intput >> cols;
+  input >> cols;
   if (!input)
   {
     std::cerr << "Cannot read file" << "\n";
