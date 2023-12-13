4cc283c277a75771137f25d794eedc1500a18378
0412erik <litsinger00@bk.ru>
2023-12-13 21:06:34 +0300

    fix file name

---
+++ litsinger.ernest/P4/functioninput.hpp
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_HPP
+#define FUNCTION_HPP
+#include <cstddef>
+#include <fstream>
+namespace litsinger
+{
+  size_t inputArray(std::ifstream & input, int * matrix, size_t sizeMatrix);
+}
+#endif
