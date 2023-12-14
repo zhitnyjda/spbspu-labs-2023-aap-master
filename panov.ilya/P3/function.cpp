9ad4b81e34c2387813ec44f47cc22f085d4f479e
ilyapanov123 <89517673116g@gmail.com>
2023-12-14 21:00:43 +0300

    P3 ff

--- panov.ilya/P3/function.cpp
+++ panov.ilya/P3/function.cpp
@@ -15,7 +15,7 @@ void function(char* input) {
 
   for (int i = 0; i < len; i++) {
     char c = input[i];
-    if (char(c) && std::isdigit && c != 'A' && c != 'O' && c != 'E' && c != 'I' && c != 'U' && c != 'Y' &&
+    if (char(c) && c != 'A' && c != 'O' && c != 'E' && c != 'I' && c != 'U' && c != 'Y' &&
       c != 'a' && c != 'o' && c != 'e' && c != 'i' && c != 'u' && c != 'y') {
       input[index++] = c;
     }
