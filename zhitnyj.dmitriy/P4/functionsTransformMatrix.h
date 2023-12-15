6a4a4c41f97909f21579f4c54317e7254270d25a
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-12-15 23:34:59 +0300

    Create functionsTransformMatrix.h

---
+++ zhitnyj.dmitriy/P4/functionsTransformMatrix.h
@@ -0,0 +1,19 @@
+#ifndef FUNCTIONS_MAX_SUM_DIAGONAL_H
+#define FUNCTIONS_MAX_SUM_DIAGONAL_H
+
+#include <fstream>
+
+struct Matrix {
+public:
+    Matrix(int r, int c);
+    ~Matrix();
+    void readFromFile(std::ifstream &infile);
+    int calculateMaxSumDiagonal() const;
+    void writeToOutput(std::ofstream &outfile) const;
+
+private:
+    int **elements;
+    int rows, cols;
+};
+
+#endif
