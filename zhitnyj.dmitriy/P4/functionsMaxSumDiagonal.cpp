8ba003849af2dd6c5e3ec6729f992d86d9a67b12
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-12-15 23:59:11 +0300

    Update functionsMaxSumDiagonal.cpp

--- zhitnyj.dmitriy/P4/functionsMaxSumDiagonal.cpp
+++ zhitnyj.dmitriy/P4/functionsMaxSumDiagonal.cpp
@@ -2,27 +2,6 @@
 #include <algorithm>
 #include <stdexcept>
 
-Matrix::Matrix(int r, int c) : rows(r), cols(c) {
-    if (rows <= 0 || cols <= 0 || rows > 100 || cols > 100) {
-        throw std::invalid_argument("Invalid matrix size.");
-    }
-    try {
-        elements = new int*[rows];
-        for (int i = 0; i < rows; ++i) {
-            elements[i] = new int[cols];
-        }
-    } catch (const std::bad_alloc&) {
-        throw std::runtime_error("Failed to allocate memory for matrix.");
-    }
-}
-
-Matrix::~Matrix() {
-    for (int i = 0; i < rows; ++i) {
-        delete[] elements[i];
-    }
-    delete[] elements;
-}
-
 void Matrix::readFromFile(std::ifstream &infile) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
