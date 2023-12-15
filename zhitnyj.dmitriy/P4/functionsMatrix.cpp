c56e9d4628fc3df08ba9c393584d8be30446d2f0
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-12-16 01:07:36 +0300

    Update functionsMatrix.cpp

--- zhitnyj.dmitriy/P4/functionsMatrix.cpp
+++ zhitnyj.dmitriy/P4/functionsMatrix.cpp
@@ -3,7 +3,7 @@
 
 Matrix::Matrix(int r, int c, int arrayType) : rows(r), cols(c), arrayType(arrayType) {
     if (arrayType == 1) {
-        if (rows <= 0 || cols <= 0 || rows > MAX_SIZE || cols > MAX_SIZE) {
+        if (rows > MAX_SIZE || cols > MAX_SIZE) {
             throw std::invalid_argument("Invalid size for static array.");
         }
     } else if (arrayType == 2) {
@@ -45,14 +45,82 @@ void Matrix::readFromFile(std::ifstream &infile) {
     }
 }
 
-int Matrix::calculateMaxSumDiagonal() const {
+void Matrix::calculateMaxSumDiagonal() const {
     int maxSum = 0;
-    // Реализация calculateMaxSumDiagonal
-    return maxSum;
+    for (int d = 0; d < cols; ++d) {
+        int sum = 0;
+        for (int i = 0, j = d; i < rows && j < cols; ++i, ++j) {
+            sum += (arrayType == 1) ? staticArray[i][j] : dynamicArray[i][j];
+        }
+        maxSum = std::max(maxSum, sum);
+    }
+    
+    for (int d = 1; d < rows; ++d) {
+        int sum = 0;
+        for (int i = d, j = 0; i < rows && j < cols; ++i, ++j) {
+            sum += (arrayType == 1) ? staticArray[i][j] : dynamicArray[i][j];
+        }
+        maxSum = std::max(maxSum, sum);
+    }
+    std::cout << maxSum << std::endl;
 }
 
 void Matrix::transformMatrix() {
-    // Реализация transformMatrix
+    int decrement = 1;
+    int startRow = 0, endRow = rows - 1;
+    int startCol = 0, endCol = cols - 1;
+
+    while (startRow <= endRow && startCol <= endCol) {
+        for (int i = startCol; i <= endCol; ++i) {
+            if (arrayType == 1) {
+                staticArray[startRow][i] -= decrement++;
+            } else {
+                dynamicArray[startRow][i] -= decrement++;
+            }
+        }
+        startRow++;
+
+        for (int i = startRow; i <= endRow; ++i) {
+            if (arrayType == 1) {
+                staticArray[i][endCol] -= decrement++;
+            } else {
+                dynamicArray[i][endCol] -= decrement++;
+            }
+        }
+        endCol--;
+
+        if (startRow <= endRow) {
+            for (int i = endCol; i >= startCol; --i) {
+                if (arrayType == 1) {
+                    staticArray[endRow][i] -= decrement++;
+                } else {
+                    dynamicArray[endRow][i] -= decrement++;
+                }
+            }
+            endRow--;
+        }
+
+        if (startCol <= endCol) {
+            for (int i = endRow; i >= startRow; --i) {
+                if (arrayType == 1) {
+                    staticArray[i][startCol] -= decrement++;
+                } else {
+                    dynamicArray[i][startCol] -= decrement++;
+                }
+            }
+            startCol++;
+        }
+    }
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (arrayType == 1) {
+                std::cout << staticArray[i][j] << " ";
+            } else {
+                std::cout << dynamicArray[i][j] << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
 }
 
 void Matrix::writeToOutput(std::ofstream &outfile) const {
