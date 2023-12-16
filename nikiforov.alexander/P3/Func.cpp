140fb726026b99bde449a90d539f2d7b80e8e01e
AlexNik <sahan@DESKTOP-R8OTJOJ>
2023-12-16 15:47:42 +0300

    fixed run without input

--- nikiforov.alexander/P3/Func.cpp
+++ nikiforov.alexander/P3/Func.cpp
@@ -1,7 +1,12 @@
 #include "Func.hpp"
+#include <iostream>
 
 size_t nikiforov::result(char* str1, char* str2, size_t len1, size_t len2) {
   size_t count = 0;
+  if (len1 == 0 || len2 == 0) 
+  {
+    throw std::overflow_error("Too short sequence\n");
+  }
   for (size_t i = 0; i < len1; i++)
   {
     for (size_t j = 0; j < len2; j++)
