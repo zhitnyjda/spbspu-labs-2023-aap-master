b94da25ac02312326d460486c6b5cb7387e1ce6b
AlexNik <sahan@DESKTOP-R8OTJOJ>
2023-12-16 16:14:55 +0300

    delete try-catch, add error without input

--- nikiforov.alexander/P3/Func.cpp
+++ nikiforov.alexander/P3/Func.cpp
@@ -1,14 +1,7 @@
 #include "Func.hpp"
-#include <iostream>
 
 size_t nikiforov::result(char* str1, char* str2, size_t len1, size_t len2) {
   size_t count = 0;
-  if (len1 == 0 || len2 == 0)
-  {
-    throw std::overflow_error("Too short sequence\n");
-    delete[] str1;
-    delete[] str2;
-  }
   for (size_t i = 0; i < len1; i++)
   {
     for (size_t j = 0; j < len2; j++)
@@ -21,6 +14,4 @@ size_t nikiforov::result(char* str1, char* str2, size_t len1, size_t len2) {
     }
   }
   return count;
-  delete[] str1;
-  delete[] str2;
 }
--- nikiforov.alexander/P3/main.cpp
+++ nikiforov.alexander/P3/main.cpp
@@ -1,46 +1,43 @@
 #include <iostream>
-#include <cstring>
-#include <iomanip>
 #include "Func.hpp"
 
 using namespace nikiforov;
 int main()
 {
-  try
+  char c = 0;
+  size_t size = 0;
+  size_t read = 0;
+  char* str1 = new char[size] {};
+  char* str2 = new char[size] {};
+  std::cin >> std::noskipws;
+
+  while ((std::cin >> c) && (c != '\n'))
   {
-    char c = 0;
-    size_t size = 0;
-    size_t read = 0;
-    char* str1 = new char[size] {};
-    char* str2 = new char[size] {};
-    std::cin >> std::noskipws;
+    nikiforov::AddElem(str1, size, read);
+    str1[read++] = c;
+  }
 
-    while ((std::cin >> c) && (c != '\n'))
-    {
-      nikiforov::AddElem(str1, size, read);
-      str1[read++] = c;
-    }
+  size_t len1 = read;
+  size = 0;
+  read = 0;
 
-    size_t len1 = read;
-    size = 0;
-    read = 0;
+  while ((std::cin >> c) && (c != '\n'))
+  {
+    nikiforov::AddElem(str2, size, read);
+    str2[read++] = c;
+  }
 
-    while ((std::cin >> c) && (c != '\n'))
-    {
-      nikiforov::AddElem(str2, size, read);
-      str2[read++] = c;
-    }
-
-    size_t len2 = read;
-    std::cin >> std::skipws;
-    std::cout << '\n' << nikiforov::result(str1, str2, len1, len2) << '\n';
+  size_t len2 = read;
+  std::cin >> std::skipws;
+  if (len1 == 0 || len2 == 0)
+  {
+    std::cerr << "Too short sequence\n";
     delete[] str1;
     delete[] str2;
-    return 0;
-  }
-  catch (const std::exception&)
-  {
-    std::cerr << "Memory allocation error!\n";
     return 1;
   }
+  std::cout << '\n' << nikiforov::result(str1, str2, len1, len2) << '\n';
+  delete[] str1;
+  delete[] str2;
+  return 0;
 }
