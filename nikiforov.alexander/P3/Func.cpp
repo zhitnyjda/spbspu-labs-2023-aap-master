448864732cae5117a726eb749fb77bfaf8fb24be
AlexNik <sahan@DESKTOP-R8OTJOJ>
2023-12-16 15:49:53 +0300

    fixed 1.4 CG

--- nikiforov.alexander/P3/Func.cpp
+++ nikiforov.alexander/P3/Func.cpp
@@ -3,7 +3,7 @@
 
 size_t nikiforov::result(char* str1, char* str2, size_t len1, size_t len2) {
   size_t count = 0;
-  if (len1 == 0 || len2 == 0) 
+  if (len1 == 0 || len2 == 0)
   {
     throw std::overflow_error("Too short sequence\n");
   }
