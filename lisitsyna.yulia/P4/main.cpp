ad14302bc3cd70b58ecb26ccd2eae5df607e6576
unknown <iulia.lis1tsyna@yandex.ru>
2023-12-16 15:43:07 +0300

    deleted lines of code

--- lisitsyna.yulia/P4/main.cpp
+++ lisitsyna.yulia/P4/main.cpp
@@ -34,11 +34,6 @@ int main(int argc, char** argv)
     std::cerr << "Cannot read the file" << "\n";
     return 2;
   }
-  if (input.eof())
-  {
-    std::cerr << "File is empty" << "\n";
-    return 2;
-  }
   if (rows < 3 || cols < 3)
   {
     std::ofstream outp(argv[3]);
