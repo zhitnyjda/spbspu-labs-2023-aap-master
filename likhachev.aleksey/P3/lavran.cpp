1e51ef555a818d798a5d6be25b0ab75fc458babb
Likhachev Aleksey <lavran03@gmail.com>
2023-12-17 22:22:56 +0300

    (fix) Доработка недоработок
    Ранее колчиество элементов и макс. количество элементов было одной переменной (баг), потому введена переменная "max_size"
    
    Действия перед итогом функции считывания были неверными, логика изменена.
    
    Добавлена очистка памяти

--- likhachev.aleksey/P3/lavran.cpp
+++ likhachev.aleksey/P3/lavran.cpp
@@ -6,55 +6,61 @@ int likhachev::readSequence(char *array)
 {
   char *reserveArray;
   char *input = array;
+  int max_size = 10;
   int size = 0;
   char c = ' ';
-  while (std::cin >> c)
+  std::cin >> std::noskipws;
+  
+  while (std::cin >> c && c != '\n')
   {
-    
-    if (c == '\n')
-    {
-      break;
-    }
-
     input[size] = c;
     size++;
 
-    if (size == size)
+    if (size == max_size)
     {
       if (input == array) // Lavran TODO: Заменить логику на матэматичэскую
       {
-        reserveArray = new char[size * 2];
+        reserveArray = new char[max_size];
+        if (!reserveArray) {
+          delete[] array;
+          delete[] reserveArray;
+          throw std::logic_error("Error: not enough space for array\n");
+        }
+
         copyCharArray(array, reserveArray, size);
         input = reserveArray;
         delete[] array;
       }
       else if (input == reserveArray)
       {
-        array = new char[size * 2];
+        array = new char[max_size];
+        if (!array) {
+          delete[] array;
+          delete[] reserveArray;
+          throw std::logic_error("Error: not enough space for array\n");
+        }
+
         copyCharArray(reserveArray, array, size);
         input = array;
         delete[] reserveArray;
       }
 
-      size *= 2;
+      max_size *= 2;
     }
     
   }
-
-  if (input == array) // Lavran TODO: Заменить логику на матэматичэскую, убрать дублирование кода
+// Lavran TODO: Заменить логику на матэматичэскую, убрать дублирование кода
+  if (input == array) 
   {
-    reserveArray = new char[size * 2];
+    reserveArray = new char[size];
     copyCharArray(array, reserveArray, size);
-    input = reserveArray;
     delete[] array;
   }
-  else if (input == reserveArray)
-  {
-    array = new char[size * 2];
-    copyCharArray(reserveArray, array, size);
-    input = array;
-    delete[] reserveArray;
-  }
+
+  array = new char[size];
+  copyCharArray(reserveArray, array, size);
+  delete[] reserveArray;
+  std::cin >> std::noskipws;
 
   return size;
 }
