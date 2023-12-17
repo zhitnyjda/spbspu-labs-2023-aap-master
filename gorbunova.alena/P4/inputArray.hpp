c8ff9b2139394c2cabc5e01ce79b39116a861525
allleng05 <allleng04@mail.ru>
2023-12-17 16:55:19 +0300

    fix

--- gorbunova.alena/P4/inputArray.hpp
+++ gorbunova.alena/P4/inputArray.hpp
@@ -5,7 +5,7 @@
 namespace gorbunova
 {
   int readFixedSizeArray(const char* fileName, int(&fixedArray)[100][100], size_t& rows, size_t& cols);
-  int readDynamicSizeArray(const char* fileName, int(**& dynamicArray, size_t& rows, size_t& cols);
+  int readDynamicSizeArray(const char* fileName, int**& dynamicArray, size_t& rows, size_t& cols);
   int writeResultToFile(const char* fileName, bool result);
 }
 #endif
