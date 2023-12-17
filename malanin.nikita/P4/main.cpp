4e12a8184c2d2fe3ff6ba6506c7d91795981671f
zeneco <nmalanin05@gmail.com>
2023-12-17 20:57:49 +0300

    P4 in process

--- malanin.nikita/P4/main.cpp
+++ malanin.nikita/P4/main.cpp
@@ -1,37 +1,5 @@
 #include <iostream>
 #include <fstream>
-int createMatrix(size_t rows, size_t cols)
-{
-  int rowsPtrs = new int * [rows];
-  for (size_t i = 0; i < rows; i++)
-  {
-    rowsPtrs[i] = nullptr;
-  }
-  try
-  {
-    for (size_t i = 0; i < rows; i++)
-    {
-      rowsPtrs[i] new int[cols];
-    }
-    return rowsPtrs;
-  }
-  catch (const std::bad_alloc & e)
-  {
-    freeMatrix(rowsPtrs, rows, cols);
-    throw;
-  }
-}
-
-void freeMatrix(int ** m, size_t rows, size_t cols)
-{
-  for (size_t i = 0;i < rows; i++)
-  {
-    delete [] m[i];
-  }
-  delete [] m;
-}
-
-
 
 int main(int argc, char ** argv)
 {
@@ -50,21 +18,21 @@ int main(int argc, char ** argv)
   catch (const std::invalid_argument & e)
   {
     std::cerr << "Can not parce a value... \n";
-    return 1;
+    return 3;
   }
 
  //argv[2] - имя файла с матрицей
-  int num = 0;
+  size_t rows = 0;
+  size_t cols = 0;
   {
     std::ifstream input(argv[2]);
-    input >> num;
+    input >> rows >> cols;
     if (!input)
     {
        std::cerr << "Can not read a number... \n";
-       return 1;
+       return 2;
     }
   }
-  std::cout << num << "\n";
 
   //argv[3] - имя файла для вывода результатов
   std::ofstream output(argv[3]);
