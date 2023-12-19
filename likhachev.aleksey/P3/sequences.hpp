dd822e686d2809df73a85747bcd2a4d99f4cdb9e
Likhachev Aleksey <lavran03@gmail.com>
2023-12-19 13:48:15 +0300

    (fix) Изменение возвращаемого значения
    Ранее функция copyCharArray возвращала беспричинно int. Теперь типом возвращаемого значения является void

--- likhachev.aleksey/P3/sequences.hpp
+++ likhachev.aleksey/P3/sequences.hpp
@@ -3,7 +3,7 @@
 namespace likhachev
 {
   int readSequence(char *array);
-  int copyCharArray(char *copyFrom, char *copyTo, int size);
+  void copyCharArray(char *copyFrom, char *copyTo, int size);
 
   int strDoesHasSame(char *array1, int size1, char *array2, int size2);
   int strRemoveDigits(char *array, int size);
