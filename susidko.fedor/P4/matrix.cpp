cabfac63e3b1fa32acef74b5f01af3fa3fb4d0f3
F-psss <fpvadimovich@gmail.com>
2023-12-20 00:56:58 +0300

    v0.15

--- susidko.fedor/P4/matrix.cpp
+++ susidko.fedor/P4/matrix.cpp
@@ -8,15 +8,15 @@ int susidko::CNT_ROW_NSM(const int * matrix, size_t rows, size_t cols)
   int flag;
   for (size_t i = 0; i < rows; i++)
   {
-	  flag = 1;
-	  for (size_t j = 1; j < cols; j++)
-	  {
+    flag = 1;
+    for (size_t j = 1; j < cols; j++)
+    {
       if(matrix[i*cols+j] == matrix[i*cols+j-1])
       {
-		    flag = 0;
-	    }
-	  }
-	  res += flag;
+        flag = 0;
+      }
+    }
+    res += flag;
   }
   return res;
 }
@@ -28,22 +28,22 @@ int susidko::NUM_COL_LSR(const int * matrix, size_t rows, size_t cols)
   int res = 0;
   for (size_t j = 0; j < cols; j++)
   {
-	  for (size_t i = 1; i < rows; i++)
-	  {
-	    counter = 0;
+    for (size_t i = 1; i < rows; i++)
+    {
+      counter = 0;
       if(matrix[i*cols+j] == matrix[(i-1)*cols+j])
       {
         counter += 1;
-	    }
-	    else
-	    {
-		    counter = 0;
-	    }
-	  }
-	  if (counter > mmax)
-	  {
-	    mmax = counter;
-	    res = j + 1;
+      }
+      else
+      {
+        counter = 0;
+      }
+    }
+    if (counter > mmax)
+    {
+      mmax = counter;
+      res = j + 1;
     }
   }
   return res;
