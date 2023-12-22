c4c504b543f9bb023fec668d7a49e5cb775e35fa
pinniks <nik.nwlife@gmail.com>
2023-12-22 22:04:16 +0300

    some fix

--- popov.nikita/P3/main.cpp
+++ popov.nikita/P3/main.cpp
@@ -1,36 +1,34 @@
 #include <iostream>
-#include "findInt.hpp"
+#include <cctype>
 
 int main()
 {
-  using namespace popov;
-  FindInt FindInteger;
   const int add = 15;
   char * input = new char[add]{};
-  char c;
   int len = 0;
   int count = 1;
   std::cin >> std::noskipws;
+  char c;
   do
   {
     std::cin >> c;
-    input[len++] = c;
+    if (isdigit(c) == 0)
+    {
+      input[len++] = c;
+    }
     if (len - (add * count) == 0)
     {
       ++count;
-      char * newinput = new char[add + len];
+      char * biggerinput = new char[add + len];
       for(int i = 0; i < len; i++)
       {
-        newinput[i] = input[i];
+        biggerinput[i] = input[i];
       }
       delete [] input;
-      input = newinput;
+      input = biggerinput;
     }
   }while (c != '\n');
   std::cin >> std::skipws;
-  char * result = new char[len]{};
-  FindInteger(result, input, &len);
-  std::cout << result;
+  std::cout << input;
   delete [] input;
-  delete [] result;
 }
