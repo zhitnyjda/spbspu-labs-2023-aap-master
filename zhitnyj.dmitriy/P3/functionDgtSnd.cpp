813761fdfc443c33f0eab0a9b5acb065cef9a1a0
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-12-23 00:05:15 +0300

    Update functionDgtSnd.cpp

--- zhitnyj.dmitriy/P3/functionDgtSnd.cpp
+++ zhitnyj.dmitriy/P3/functionDgtSnd.cpp
@@ -4,26 +4,26 @@
 
 char* appendNumToStr(char* str1, char* str2)
 {
-		char* dest = new char();
-		char* dest_ = dest;
+    char* dest = new char();
+    char* dest_ = dest;
 
-		while (*str1)
-		{
-				*dest++ = *str1++;
-		}
+    while (*str1)
+    {
+        *dest++ = *str1++;
+    }
 
-		std::cout << dest << std::endl;
+    std::cout << dest << std::endl;
 
-		while (*str2)
-		{
-				if (std::isdigit(*str2))
-				{
-						*dest++ = *str2;
-				}
-				++str2;
-		}
+    while (*str2)
+    {
+        if (std::isdigit(*str2))
+        {
+            *dest++ = *str2;
+        }
+        ++str2;
+    }
 
-		*dest += '\0';
+    *dest += '\0';
 
-		return dest_;
+    return dest_;
 }
