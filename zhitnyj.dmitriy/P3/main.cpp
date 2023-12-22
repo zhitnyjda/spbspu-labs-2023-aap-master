0296f242af75d2e2a8e034ace0adc0a616286eaa
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-12-23 00:15:00 +0300

    Update main.cpp

--- zhitnyj.dmitriy/P3/main.cpp
+++ zhitnyj.dmitriy/P3/main.cpp
@@ -11,8 +11,18 @@ int main()
     {
         std::cin >> str >> str_;
 
+        if(!*str || !*str_)
+        {
+            throw std::logic_error("Input error!");
+        }
+
         std::cout << strSymbolsRepeats(str) << std::endl;
         std::cout << appendNumToStr(str, str_) << std::endl;
+    }
+    catch(const std::logic_error& err)
+    {
+        std::cerr << err.what() << std::endl;
+        return 1;
     }
     catch (const std::exception& err)
     {
