4590d217355509decde635a6eec2501b26145a3b
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-12-23 00:04:49 +0300

    Update functionSeqSym.cpp

--- zhitnyj.dmitriy/P3/functionSeqSym.cpp
+++ zhitnyj.dmitriy/P3/functionSeqSym.cpp
@@ -2,16 +2,16 @@
 
 size_t strSymbolsRepeats(char* str)
 {
-		for (size_t i = 0; str[i] != '\0'; ++i)
-		{
-				for (size_t j = i + 1; str[j] != '\0'; ++j)
-				{
-						if (str[i] == str[j])
-						{
-								return 1;
-						}
-				}
-		}
+    for (size_t i = 0; str[i] != '\0'; ++i)
+    {
+        for (size_t j = i + 1; str[j] != '\0'; ++j)
+        {
+            if (str[i] == str[j])
+            {
+                return 1;
+            }
+        }
+    }
 
-		return 0;
+    return 0;
 }
