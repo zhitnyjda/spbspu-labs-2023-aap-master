3ca338aef96e1e37dad66649518f983392377082
kh-alisa <alisakhomicheko@gmail.com>
2023-12-22 23:49:30 +0300

    improved

--- khomichenko.alisa/P3/main.cpp
+++ khomichenko.alisa/P3/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <exception>
+#include <cctype>
 #include "readString.hpp"
 #include "varFunc.hpp"
 
@@ -11,17 +12,22 @@ int main()
   char c = 0;
   int read = 0;
   std::cin >> std::noskipws;
+  if (!std::cin)
+  {
+    std::cerr<<"input failed\n";
+    return 1;
+  }
   while ((std::cin >> c) && (read < size))
   {
-    firstInput[read++] = c;
+    firstInput [read++] = c;
     if (c == '\n')
     {
-      firstInput[read - 1] = 0;
+      firstInput [read - 1] = 0;
       break;
     }
     try
     {
-      firstInput =(c != '\n' && read == size)? khomichenko::createBiggerString (&size, firstInput): firstInput;
+      firstInput = (c != '\n' && read == size)? khomichenko::createBiggerString (&size, firstInput) : firstInput;
     }
     catch (std::bad_alloc & e)
     {
@@ -29,15 +35,20 @@ int main()
       return 1;
     }
   }
+  if (read == 1)
+  {
+    std::cerr << "empty input\n";
+    return 1;
+  }
   std::cin >> std::skipws;
   //DIF-LAT
-  std::cout<<khomichenko::countDifLat(firstInput, size)<<"\n";
-  char result[size] ={};
+  std::cout << khomichenko::countDifLat(firstInput, size)<<"\n";
+  char result [size] = {};
   //RMV-VOW
   khomichenko::makingRmvVow(firstInput, size, result);
   for (int i = 0; i < size; i++)
   {
-    std::cout<<result[i];
+    std::cout << result[i];
   }
   std::cout<<"\n";
   delete [] firstInput;
--- khomichenko.alisa/P3/readString.cpp
+++ khomichenko.alisa/P3/readString.cpp
@@ -4,17 +4,17 @@ void khomichenko::swapStrings ( char * first, char * second, int size)
 {
   for (int i = 0; i < size;  i++)
   {
-    second[i]=first[i];
+    second[i] = first[i];
   }
 }
 
 char * khomichenko::createBiggerString (int * size, char * first)
 {
-  char * secondInput = new char [(*size)+(*size)]{};
+  char * secondInput = new char [(*size) + (*size)] {};
   swapStrings(first, secondInput, (*size));
-  (*size)+=(*size);
+  (*size) += (*size);
   delete [] first;
-  first = new char [(*size)]{};
+  first = new char [(*size)] {};
   swapStrings(secondInput, first, (*size));
   delete [] secondInput;
   return first;
