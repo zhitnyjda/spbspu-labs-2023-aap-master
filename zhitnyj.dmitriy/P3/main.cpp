95791f23f9973045b79de96be8106070dd17ccff
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-12-23 00:50:43 +0300

    Update main.cpp

--- zhitnyj.dmitriy/P3/main.cpp
+++ zhitnyj.dmitriy/P3/main.cpp
@@ -5,35 +5,33 @@
 
 int main()
 {
-  char* str = readDynamicString();
-  char* str_ = readDynamicString();
+    char* str = readDynamicString();
+    char* str_ = readDynamicString();
 
-  try
-  {
-    std::cin >> str >> str_;
+    try
+    {
+        std::cout << strSymbolsRepeats(str) << std::endl;
+        std::cout << appendNumToStr(str, str_) << std::endl;
 
-    std::cout << strSymbolsRepeats(str) << std::endl;
-    std::cout << appendNumToStr(str, str_) << std::endl;
+        delete[] str;
+        delete[] str_;
+    }
+    catch (const std::logic_error& err)
+    {
+        std::cerr << err.what() << std::endl;
 
-    delete[] str;
-    delete[] str_;
-  }
-  catch (const std::logic_error& err)
-  {
-    std::cerr << err.what() << std::endl;
+        delete[] str;
+        delete[] str_;
+        return 1;
+    }
+    catch (const std::exception& err)
+    {
+        std::cerr << err.what() << std::endl;
 
-    delete[] str;
-    delete[] str_;
-    return 1;
-  }
-  catch (const std::exception& err)
-  {
-    std::cerr << err.what() << std::endl;
+        delete[] str;
+        delete[] str_;
+        return 1;
+    }
 
-    delete[] str;
-    delete[] str_;
-    return 1;
-  }
-
-  return 0;
+    return 0;
 }
