253c23cccb6de1577e0d8e9657bcad8eb84f1690
AlexNik <sahan@DESKTOP-R8OTJOJ>
2023-12-23 22:01:35 +0300

    changing char to constant, initializing char

--- nikiforov.alexander/P3/Func.cpp
+++ nikiforov.alexander/P3/Func.cpp
@@ -1,6 +1,6 @@
 #include "Func.hpp"
 
-size_t nikiforov::result(char* str1, char* str2, size_t len1, size_t len2) {
+size_t nikiforov::result(char* str1, const char* str2, size_t len1, size_t len2) {
   size_t count = 0;
   for (size_t i = 0; i < len1; i++)
   {
--- nikiforov.alexander/P3/main.cpp
+++ nikiforov.alexander/P3/main.cpp
@@ -8,7 +8,7 @@ int main()
   size_t size = 0;
   size_t read = 0;
   char* str1 = new char[size] {};
-  char* str2 = new char[size] {};
+  const char* str2 = "abs";
   std::cin >> std::noskipws;
 
   while ((std::cin >> c) && (c != '\n'))
@@ -17,26 +17,18 @@ int main()
     str1[read++] = c;
   }
 
-  size_t len1 = read;
+  size_t len1 = strlen(str1);
   size = 0;
-  read = 0;
 
-  while ((std::cin >> c) && (c != '\n'))
-  {
-    nikiforov::AddElem(str2, size, read);
-    str2[read++] = c;
-  }
-  size_t len2 = read;
+  size_t len2 = strlen(str2);
   std::cin >> std::skipws;
   if (len1 == 0 && len2 == 0)
   {
     std::cerr << "Too short sequence\n";
     delete[] str1;
-    delete[] str2;
     return 1;
   }
   std::cout << '\n' << nikiforov::result(str1, str2, len1, len2) << '\n';
   delete[] str1;
-  delete[] str2;
   return 0;
 }
