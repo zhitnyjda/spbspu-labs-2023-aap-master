05531c3bb4782af575d181c6ea0e38023a35fb12
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-12-23 04:51:05 +0300

    Update functionsMatrix.cpp

--- zhitnyj.dmitriy/P4/functionsMatrix.cpp
+++ zhitnyj.dmitriy/P4/functionsMatrix.cpp
@@ -4,8 +4,7 @@
 #include <cstdlib>
 #include <stdexcept>
 
-Matrix::Matrix(int r, int c, int arrayType) : rows(r), cols(c), arrayType(arrayType)
-{
+Matrix::Matrix(int r, int c, int arrayType) : rows(r), cols(c), arrayType(arrayType) {
     if (arrayType == 1) {
         if (rows > MAX_SIZE || cols > MAX_SIZE) {
             throw std::invalid_argument("Invalid size for static array.");
@@ -17,8 +16,7 @@ Matrix::Matrix(int r, int c, int arrayType) : rows(r), cols(c), arrayType(arrayT
     }
 }
 
-Matrix::~Matrix()
-{
+Matrix::~Matrix() {
     if (arrayType == 2) {
         deallocateDynamicArray();
     }
