9205463b32ce7e8f4126e87b9e865ded8a477d9a
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-12-23 09:05:57 +0300

    Update matrix.h

--- zhitnyj.dmitriy/P4/matrix.h
+++ zhitnyj.dmitriy/P4/matrix.h
@@ -1,6 +1,5 @@
 #ifndef MATRIX_H
 #define MATRIX_H
-#include <memory>
 #include <cstdio>
 
 class Matrix
@@ -8,7 +7,6 @@ class Matrix
 public:
     Matrix();
     Matrix(int rows, int cols);
-    Matrix(const Matrix& matrix);
     ~Matrix();
     void loadFromFile(char* filename);
     void saveToFile(char* filename);
@@ -17,7 +15,7 @@ public:
 
 private:
     int rows, cols;
-    std::unique_ptr<int[]> data;
+    int** data;
 
     void allocateMemory();
     void freeMemory();
