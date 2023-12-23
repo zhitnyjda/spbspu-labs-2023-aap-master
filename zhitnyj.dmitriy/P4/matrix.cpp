dd226eceb8b7f40b50a16017d4d840492d7fb3b5
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2023-12-23 13:52:10 +0300

    Update matrix.cpp

--- zhitnyj.dmitriy/P4/matrix.cpp
+++ zhitnyj.dmitriy/P4/matrix.cpp
@@ -13,7 +13,10 @@ Matrix::Matrix(int rows, int cols, int num) : rows(rows), cols(cols), num_(num),
 
 Matrix::~Matrix()
 {
-  freeMemory();
+  if (data != nullptr)
+  {
+    freeMemory();
+  }
 }
 
 void Matrix::allocateMemory()
