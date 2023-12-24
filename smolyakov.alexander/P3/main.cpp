6511d3540d3c59c37efebf62db9ca0b6f24fb937
Ignitron1 <iamremotr@gmail.com>
2023-12-24 05:18:07 -0500

    remade P3

--- smolyakov.alexander/P3/main.cpp
+++ smolyakov.alexander/P3/main.cpp
@@ -1,40 +1,18 @@
 #include <iostream>
-#include "repDgt.hpp"
+#include "dynamicStringReader.hpp"
+#include "repeatingDigitsCheck.hpp"
 
 int main()
 {
-  const int SIZE_INCREMENT = 10;
-  const char * BAD_INPUT_ALLOC = "Could not allocate memory for an input string.\n";
-  char currentCharacter = 0;
-  size_t read = 0;
-  char * inputString = new char[SIZE_INCREMENT]{};
-
-  do
+  using namespace cringecode;
+  DynamicStringReader stringReader = DynamicStringReader(10);
+  char* string = stringReader.read(std::cin);
+  if (containsRepeatingDigits(string, stringReader.getLength()))
   {
-    std::cin.get(currentCharacter);
-    inputString[read++] = currentCharacter;
-    if (read % SIZE_INCREMENT == 0)
-    {
-      try
-      {
-	char * tmpInputString = new char[read + SIZE_INCREMENT]{};
-        for (size_t i = 0; i < read; i++)
-        {
-     	  tmpInputString[i] = inputString[i];
-        }
-        delete[] inputString;
-        inputString = tmpInputString;
-      }
-      catch (const std::bad_alloc & e)
-      {
-	std::cerr << BAD_INPUT_ALLOC;
-	return 1;
-      }
-    }
+    std::cout << "Ja" << '\n';
   }
-  while (currentCharacter != '\n');
-
-  std::cout << containsRepeatingDigits(inputString, read) << '\n'; 
-  delete[] inputString;
-  return 0;
+  else
+  {
+    std::cout << "Nein" << '\n';
+  }
 }
---
+++ smolyakov.alexander/P3/repeatingDigitsCheck.cpp
@@ -0,0 +1,20 @@
+#include "repeatingDigitsCheck.hpp"
+#include <cctype>
+
+int containsRepeatingDigits(const char* string, std::size_t length)
+{
+  int digitsCount[10] = {0};
+
+  for (size_t i = 0; i < length; i++)
+  {
+    if (std::isdigit(string[i]))
+    {
+      int digit = string[i] - '0';
+      if (digitsCount[digit]++ > 0)
+      {
+	return 1;
+      }
+    }
+  }
+  return 0;
+}
