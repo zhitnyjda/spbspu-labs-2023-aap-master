e99c5cc9c93df8c34f11f91ffe51a120d9be2d68
GuyFawkes00 <spproselkov@mail.ru>
2023-12-26 21:24:37 +0300

    error processing

--- proselkov.stanislav/P3/main.cpp
+++ proselkov.stanislav/P3/main.cpp
@@ -5,13 +5,23 @@
 
 int main()
 {
-  char * input1 = nullptr;
   const char * input2 = "abc_def12";
-  char * output = nullptr;
-  std::cout <<//
-  std::cout <<//
-  std::cout <<//OUTPUT
+  try
+  {
+    char * input1 = new char[10]{};
+    char * output = new char[10]{};
+    std::cout <<//
+    std::cout <<//
+    std::cout <<//OUTPUT
 
-  delete[] input1
-  delete[] input2
-  delete[] output
+    delete[] input1
+    delete[] input2
+    delete[] output
+    return 0;
+  }
+  catch (const std::bad_alloc & e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+}
