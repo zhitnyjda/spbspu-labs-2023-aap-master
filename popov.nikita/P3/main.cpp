7ccc371b2dd23f2b0de2567d351f2a7b6ea4ffab
pinniks <nik.nwlife@gmail.com>
2023-12-26 23:31:30 +0300

    new function has been added

--- popov.nikita/P3/main.cpp
+++ popov.nikita/P3/main.cpp
@@ -1,36 +1,15 @@
 #include <iostream>
-#include <cctype>
 #include "noInt.hpp"
+#include "newLine.hpp"
 
 int main()
 {
   using namespace popov;
-  const int add = 15;
-  char * input = new char[add]{};
   int len = 0;
-  int count = 1;
-  std::cin >> std::noskipws;
-  char c;
-  do
-  {
-    std::cin >> c;
-    input[len++] = c;
-    if (len - (add * count) == 0)
-    {
-      ++count;
-      char * biggerinput = new char[add + len];
-      for(int i = 0; i < len; i++)
-      {
-        biggerinput[i] = input[i];
-      }
-      delete [] input;
-      input = biggerinput;
-    }
-  }while (c != '\n');
-  std::cin >> std::skipws;
+  char * input = newLine(len);
   char * res = new char[len]{};
   noInt(res, input, len);
-  std::cout << res;
+  std::cout << res << '\n';
   delete [] input;
   delete [] res;
   return 0;
