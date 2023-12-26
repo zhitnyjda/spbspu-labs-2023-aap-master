e1ebe46b9a2d65ef8a0c92deabece4f8b9058762
anyadg <anna_giraffe@mail.ru>
2023-12-26 21:59:00 +0300

    Fixed removeDup function

--- doroshenko.anna/P3/functions.hpp
+++ doroshenko.anna/P3/functions.hpp
@@ -1,10 +1,13 @@
 #ifndef RESIZE_ARRAY_HPP
 #define RESIZE_ARRAY_HPP
 #include <cstddef>
+#include <istream>
 
 namespace doroshenko
 {
   char* resizeArray(char*, size_t, size_t);
+  void removeDup(char* input, size_t readEl);
+  size_t readInput(char* input, std::istream& in);
 }
 
 #endif
--- doroshenko.anna/P3/main.cpp
+++ doroshenko.anna/P3/main.cpp
@@ -5,55 +5,13 @@
 
 int main()
 {
-  char ch = 0;
-  size_t readEl = 0;
-  size_t size = 20;
-  char* input = new char [size] {};
+  char* input = new char[20];
   try
   {
+    size_t readEl = 0;
     std::cin >> std::noskipws;
-    while (std::cin >> ch)
-    {
-      if (readEl < size)
-      {
-        input[readEl++] = ch;
-        if (ch == '\n')
-        {
-          input[readEl - 1] = 0;
-          break;
-        }
-      }
-      else
-      {
-        input = doroshenko::resizeArray(input, size, size + 20);
-        size += 20;
-        input[readEl++] = ch;
-      }
-    }
-    if (readEl == 0)
-    {
-      std::cerr << "There is no elements in the string\n";
-      delete[] input;
-      return 1;
-    }
-    bool pair = false;
-    for (size_t i = 0; i < readEl - 1; i++)
-    {
-      for (size_t j = i + 1; j < readEl; j++)
-      {
-        if (input[i] == input[j])
-        {
-          pair = true;
-          input[j] = '\0';
-        }
-      }
-      if (pair == true)
-      {
-        std::cout << input[i];
-        input[i] = '\0';
-        pair = false;
-      }
-    }
+    readEl = doroshenko::readInput(input, std::cin);
+    doroshenko::removeDup(input, readEl);
     std::cin >> std::skipws;
     std::cout << input << '\n';
     delete[] input;
@@ -64,5 +22,11 @@ int main()
     std::cerr << "Allocation error\n";
     delete[] input;
     return 1;
+  }
+  catch (const std::logic_error& e)
+  {
+      std::cerr << e.what();
+      delete[] input;
+      return 1;
   }
 }
