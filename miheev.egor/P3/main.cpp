8e3ee9451129b1a0d95b67b05f9585d03c8a9499
Miheev Egor <etdiary1@gmail.com>
2024-01-04 00:13:59 +0300

    added empty line processing

--- miheev.egor/P3/main.cpp
+++ miheev.egor/P3/main.cpp
@@ -10,6 +10,13 @@ int main()
   char* string = new char[size]{};
 
   miheev::readString(&string, size);
+  if (string[0] == '\0')
+  {
+    std::cerr << "you've entered an empty line\n";
+    delete[] string;
+    return 1;
+  }
   std::cout << miheev::countDiffLat(string) << '\n';
   delete[] string;
+  return 0;
 }
