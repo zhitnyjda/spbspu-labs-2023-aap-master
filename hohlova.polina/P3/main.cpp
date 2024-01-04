389bce4d0aebb4a059d60d35b6427311d1079f45
phohlova <pv.hohlova@edu.spbstu.ru>
2024-01-04 18:02:03 +0300

    -

--- hohlova.polina/P3/main.cpp
+++ hohlova.polina/P3/main.cpp
@@ -9,17 +9,15 @@ int main()
   using namespace hohlova;
   size_t maxCharNum = 10;
   size_t size = 0;
-  char* inputStr = nullptr;
-  try
+  char* inputStr = hohlova::readLine(std::cin, size, maxCharNum);
+  size_t inputLength = strlen(inputStr);
+  if (inputLength == 0)
   {
-    inputStr = hohlova::readLine(std::cin, size, maxCharNum);
-  }
-  catch (const std::invalid_argument & ex)
-  {
-    std::cerr << ex.what();
+    std::cerr << "error: empty line\n";
+    delete[] inputStr;
     return 1;
   }
-  std::cout << hohlova::findCommonChars(inputStr);
+  std::cout << findCommandChars(inputStr);
   delete[] inputStr;
   return 0;
 }
