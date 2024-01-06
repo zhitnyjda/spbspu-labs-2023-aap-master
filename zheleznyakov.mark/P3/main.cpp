55ea837eff8ccd7fd807b0f72e08fa9fbd1ec01a
Mark Zheleznyakov <mrqiz@internet.ru>
2024-01-06 09:58:10 +0300

    fix: Очистка dest

--- zheleznyakov.mark/P3/main.cpp
+++ zheleznyakov.mark/P3/main.cpp
@@ -6,11 +6,11 @@ int main()
 {
   try
   {
-    char *str1 = new char[10]{};
+    char *str1 = new char [1000];
     size_t len1 = zheleznyakov::readLine(str1);
     const char *str2 = "abc ef";
     size_t len2 = 6;
-    char *dest = new char[10]{};
+    char *dest = new char [1000];
     size_t destSize = zheleznyakov::findNonDuplicateChars(dest, str1, str2, len1, len2);
     for (size_t i = 0; i < destSize; i++)
     {
@@ -18,6 +18,7 @@ int main()
     }
     std::cout << '\n';
     delete[] str1;
+    delete[] dest;
     return 0;
   }
   catch (const std::bad_alloc &e)
