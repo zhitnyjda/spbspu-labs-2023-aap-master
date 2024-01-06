7766fcd54e8e74c73fe838989e9dc04ea971ead7
Mark Zheleznyakov <mrqiz@internet.ru>
2024-01-06 10:20:12 +0300

    feat: Удаление латинских букв из строки (LAT-RMV)

--- zheleznyakov.mark/P3/main.cpp
+++ zheleznyakov.mark/P3/main.cpp
@@ -6,19 +6,28 @@ int main()
 {
   try
   {
-    char *str1 = new char [1000];
+    char *str1 = new char[1000];
     size_t len1 = zheleznyakov::readLine(str1);
+
     const char *str2 = "abc ef";
     size_t len2 = 6;
-    char *dest = new char [1000];
-    size_t destSize = zheleznyakov::findNonDuplicateChars(dest, str1, str2, len1, len2);
-    for (size_t i = 0; i < destSize; i++)
+    char *nonDuplicateDest = new char[1000];
+    size_t nonDuplicateDestSize = zheleznyakov::findNonDuplicateChars(nonDuplicateDest, str1, str2, len1, len2);
+    for (size_t i = 0; i < nonDuplicateDestSize; i++)
     {
-      std::cout << dest[i];
+      std::cout << nonDuplicateDest[i];
     }
     std::cout << '\n';
+    delete[] nonDuplicateDest;
+    char *removeLettersDest = new char[1000];
+    size_t removeLettersDestSize = zheleznyakov::removeLetters(removeLettersDest, str1, len1);
+    for (size_t i = 0; i < removeLettersDestSize; i++)
+    {
+      std::cout << removeLettersDest[i];
+    }
+    std::cout << '\n';
+    delete[] removeLettersDest;
     delete[] str1;
-    delete[] dest;
     return 0;
   }
   catch (const std::bad_alloc &e)
